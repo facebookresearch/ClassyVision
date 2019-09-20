911e5d69b8ec8000ecc8dc54bfb1b1eb6028c55b
Vinicius Reis <vini@fb.com>
2019-09-20 11:06:07 -0700

    Add copyright headers
    Summary: Needed for the OSS release
    
    Differential Revision: D17504533
    
    fbshipit-source-id: 97fc2ebf4f457864f7fcfdfcafa7543e881e503a

--- classy_vision/models/__init__.py
+++ classy_vision/models/__init__.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 from collections import defaultdict
 from pathlib import Path
 
