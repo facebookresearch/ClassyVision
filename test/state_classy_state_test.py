8509e0316654fe3c96a2ddb029fff77909535213
Vinicius Reis <vini@fb.com>
2019-09-20 13:55:03 -0700

    More copyright headers (#6)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/6
    
    I missed the test directory and files in the root directory :(
    
    Reviewed By: mannatsingh
    
    Differential Revision: D17507489
    
    fbshipit-source-id: 0564ffa81df35443f89422fe1813d68c8682652a

--- test/state_classy_state_test.py
+++ test/state_classy_state_test.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 import unittest
 from test.generic.config_utils import get_test_args, get_test_task_config
 from test.generic.utils import compare_model_state, compare_samples
