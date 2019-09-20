5473afed0b589628e55f7705079983a6d5611162
Vinicius Reis <vini@fb.com>
2019-09-19 18:41:41 -0700

    Add setup.py (#2)
    Summary:
    Add basic support for packaging the project. This will be used to run ours tests with pip as well.
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/2
    
    Test Plan: python3 setup.py sdist bdist_wheel
    
    Differential Revision: D17490325
    
    Pulled By: vreis
    
    fbshipit-source-id: a3fe2a4cfe8b943bfc0922b1ad1eb2daa5802584

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+from setuptools import setup, find_packages
+import sys
+
+if sys.version_info < (3, 6):
+    sys.exit("Sorry, Python >=3.6 is required for Classy Vision.")
+
+with open("README.md", encoding="utf8") as f:
+    readme = f.read()
+
+with open("LICENSE") as f:
+    license = f.read()
+
+with open("requirements.txt") as f:
+    reqs = f.read()
+
+setup(
+    name="ClassyVision",
+    version="0.1.0",
+    description="An end-to-end computer vision framework.",
+    long_description=readme,
+    url="TBD",
+    license=license,
+    python_requires=">=3.6",
+    packages=find_packages(exclude=("configs", "tests")),
+    install_requires=reqs.strip().split("\n"),
+    include_package_data=True,
+)
