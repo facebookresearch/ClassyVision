8faa9024bcfbcaac86e335b675aa7521709677fa
Vinicius Reis <vini@fb.com>
2019-09-20 13:55:03 -0700

    More copyright headers (#6)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/6
    
    I missed the test directory and files in the root directory :(
    
    Reviewed By: mannatsingh
    
    Differential Revision: D17507489
    
    fbshipit-source-id: 0564ffa81df35443f89422fe1813d68c8682652a

--- test/meters_recall_meter_test.py
+++ test/meters_recall_meter_test.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 import torch
 from classy_vision import meters
 from classy_vision.meters.recall_meter import RecallAtKMeter
