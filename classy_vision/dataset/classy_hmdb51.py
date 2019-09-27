58eb292fc0af3c5bff5ba8dc30200619e846611c
Zhicheng Yan <zyan3@fb.com>
2019-09-26 23:14:14 -0700

    add classy_hmdb51 dataset
    Differential Revision: D16999375
    
    fbshipit-source-id: a881da867fd64d0d753bde3009ca5338ea8d1b14

---
+++ classy_vision/dataset/classy_hmdb51.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import torch
+from torchvision.datasets.hmdb51 import HMDB51
+
+from . import register_dataset
+from .classy_video_dataset import ClassyVideoDataset
+from .core import WrapTorchVisionVideoDataset
+from .transforms.util_video import build_video_field_transform_default
+
+
+@register_dataset("hmdb51")
+class HMDB51Dataset(ClassyVideoDataset):
+    def __init__(self, config):
+        super(HMDB51Dataset, self).__init__(config)
+
+        (
+            transform_config,
+            batchsize_per_replica,
+            shuffle,
+            num_samples,
+            video_width,
+            video_height,
+            video_min_dimension,
+            audio_samples,
+            step_between_clips,
+            frame_rate,
+            precomputed_metadata_filepath,
+            save_metadata_filepath,
+        ) = self.parse_config(config)
+
+        assert "splits_dir" in config
+
+        metadata = None
+        if precomputed_metadata_filepath is not None:
+            metadata = self.load_metadata(precomputed_metadata_filepath)
+
+        dataset = HMDB51(
+            config["video_dir"],
+            config["splits_dir"],
+            config["frames_per_clip"],
+            step_between_clips=step_between_clips,
+            frame_rate=frame_rate,
+            _precomputed_metadata=metadata,
+            fold=config["fold"] if "fold" in config else 1,
+            train=True if self._split is not None and self._split == "train" else False,
+            num_workers=torch.get_num_threads(),
+            _video_width=video_width,
+            _video_height=video_height,
+            _video_min_dimension=video_min_dimension,
+            _audio_samples=audio_samples,
+        )
+        self.metadata = dataset.metadata
+        if self.metadata and save_metadata_filepath:
+            self.save_metadata(save_metadata_filepath)
+
+        dataset = WrapTorchVisionVideoDataset(dataset)
+
+        transform = build_video_field_transform_default(transform_config, self._split)
+        self.dataset = self.wrap_dataset(
+            dataset,
+            transform,
+            batchsize_per_replica,
+            shuffle=shuffle,
+            subsample=num_samples,
+        )
--- classy_vision/dataset/classy_video_dataset.py
+++ classy_vision/dataset/classy_video_dataset.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
+import logging
+import os
 
+import torch
 
 from .classy_dataset import ClassyDataset
 
@@ -16,16 +19,14 @@ class ClassyVideoDataset(ClassyDataset):
         for config_key in ["frames_per_clip", "video_dir"]:
             assert config_key in config, "%s must be set" % config_key
 
-        step_between_clips = (
-            config["step_between_clips"] if "step_between_clips" in config else 1
-        )
-        frame_rate = config["frame_rate"] if "frame_rate" in config else None
-        precomputed_metadata_filepath = (
-            config["load_metadata_file"] if "load_metadata_file" in config else None
-        )
-        save_metadata_filepath = (
-            config["save_metadata_file"] if "save_metadata_file" in config else None
-        )
+        video_width = config.get("video_width", 0)
+        video_height = config.get("video_height", 0)
+        video_min_dimension = config.get("video_min_dimension", 0)
+        audio_samples = config.get("audio_samples", 0)
+        step_between_clips = config.get("step_between_clips", 1)
+        frame_rate = config.get("frame_rate", None)
+        precomputed_metadata_filepath = config.get("load_metadata_file", None)
+        save_metadata_filepath = config.get("save_metadata_file", None)
 
         (transform_config, batchsize_per_replica, shuffle, num_samples) = super(
             ClassyVideoDataset, self
@@ -36,8 +37,31 @@ class ClassyVideoDataset(ClassyDataset):
             batchsize_per_replica,
             shuffle,
             num_samples,
+            video_width,
+            video_height,
+            video_min_dimension,
+            audio_samples,
             step_between_clips,
             frame_rate,
             precomputed_metadata_filepath,
             save_metadata_filepath,
         )
+
+    def load_metadata(self, filepath):
+        assert os.path.exists(filepath), "File not found: %s" % filepath
+        metadata = torch.load(filepath)
+        return metadata
+
+    def save_metadata(self, filepath):
+        filedir = os.path.dirname(filepath)
+        if not os.path.exists(filedir):
+            try:
+                os.mkdirs(filedir)
+            except Exception:
+                logging.warn("Fail to create folder: %s" % filedir)
+                return
+        logging.info("Save metadata to file: %s" % filepath)
+        try:
+            torch.save(self.metadata, filepath)
+        except ValueError:
+            logging.warn("Fail to save metadata to file: %s" % filepath)
