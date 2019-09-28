4ed2848b5ee4f934594ba1e416abff701e6dbfdf
Simran Suresh Motwani <simranmotwani@fb.com>
2019-09-28 00:34:43 -0700

    Fix Head Imports (#27)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/27
    
    Fixed classy vision head imports
    
    Reviewed By: vreis
    
    Differential Revision: D17635517
    
    fbshipit-source-id: 0dd0eb73bb3d03cd442de35ba893d78a7a9180d5

--- classy_vision/heads/identity_head.py
+++ classy_vision/heads/identity_head.py
@@ -4,8 +4,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from classy_vision.heads import register_head
-from classy_vision.heads.classy_vision_head import ClassyVisionHead
+from classy_vision.heads import ClassyVisionHead, register_head
 
 
 @register_head("identity")
