750b447505e1fda7be50db6df812551e6aa4735f
Vinicius Reis <vini@fb.com>
2019-09-30 20:26:14 -0700

    Create DistributedTrainer (#31)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/31
    
    Create a DistributedTrainer class to take care of the initial setup of
    environment variables for distributed training. This greatly simplifies the
    code in classy_train.py, and moves us a bit closer to the vision in n128788
    
    Reviewed By: mannatsingh
    
    Differential Revision: D17661068
    
    fbshipit-source-id: 5cdf966dc3f8d78db99c141441a988ce946e1f5d

--- classy_train.py
+++ classy_train.py
@@ -8,12 +8,6 @@ import logging
 import os
 
 import torch
-from classy_vision.generic.distributed_util import (
-    get_rank,
-    get_world_size,
-    set_cpu_device,
-    set_cuda_device_index,
-)
 from classy_vision.generic.opts import parse_train_arguments
 from classy_vision.generic.util import load_checkpoint, load_json
 from classy_vision.hooks import (
@@ -27,33 +21,7 @@ from classy_vision.hooks import (
     VisdomHook,
 )
 from classy_vision.tasks import setup_task
-from classy_vision.trainer import ClassyTrainer
-
-
-def _init_env_vars():
-    if "WORLD_SIZE" not in os.environ or "RANK" not in os.environ:
-        os.environ["WORLD_SIZE"] = "1"
-        os.environ["RANK"] = "0"
-        os.environ["LOCAL_RANK"] = "0"
-
-    if "MASTER_ADDR" not in os.environ or "MASTER_PORT" not in os.environ:
-        os.environ["MASTER_ADDR"] = "127.0.0.1"
-        os.environ["MASTER_PORT"] = "29500"
-
-
-def _init_distributed(device):
-    """ perform distributed setup, requires the script to be started with
-        torch.distributed.launch script.
-    """
-    distributed_world_size = int(os.environ["WORLD_SIZE"])
-    distributed_rank = int(os.environ["RANK"])
-    backend = "nccl" if device == "gpu" else "gloo"
-    torch.distributed.init_process_group(
-        backend=backend,
-        init_method="env://",
-        world_size=distributed_world_size,
-        rank=distributed_rank,
-    )
+from classy_vision.trainer import DistributedTrainer
 
 
 # run all the things:
@@ -63,21 +31,6 @@ def main(args):
     # Loads config, sets up task
     config = load_json(args.config)
 
-    _init_env_vars()
-    _init_distributed(args.device)
-
-    logging.info(
-        "Done setting up distributed process_group with rank {}, world_size {}".format(
-            get_rank(), get_world_size()
-        )
-    )
-    local_rank = int(os.environ["LOCAL_RANK"])
-    if args.device == "gpu":
-        logging.info("Using GPU, CUDA device index: {}".format(local_rank))
-        set_cuda_device_index(local_rank)
-    else:
-        logging.info("Using CPU")
-        set_cpu_device()
     task = setup_task(config, args)
 
     # Load checkpoint, if available
@@ -101,7 +54,9 @@ def main(args):
     if args.visdom_server != "":
         hooks.append(VisdomHook(args.visdom_server, args.visdom_port))
 
-    trainer = ClassyTrainer(hooks, args.device == "gpu", num_workers=args.num_workers)
+    trainer = DistributedTrainer(
+        hooks, args.device == "gpu", num_workers=args.num_workers
+    )
     trainer.train(task)
 
 
