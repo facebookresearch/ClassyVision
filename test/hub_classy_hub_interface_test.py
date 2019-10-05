0068ff543b36826cd49a0866abde8d8903641f99
Mannat Singh <mannatsingh@fb.com>
2019-10-04 21:52:13 -0700

    Classy model wrapper (#42)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/42
    
    Created a `ClassyModelWrapper` which wraps an `nn.Module` by a `ClassyVisionModel`, so that the model can be used in Classy Vision.
    
    This wrapper is used in `ClassyHubInterface` so that `from_model()` can support any general models.
    
    Reviewed By: vreis
    
    Differential Revision: D17767522
    
    fbshipit-source-id: c80d7a0ee5523b313491a8c3e158ce49c348c5f3

--- test/hub_classy_hub_interface_test.py
+++ test/hub_classy_hub_interface_test.py
@@ -13,7 +13,7 @@ import torch
 from classy_vision.hub import ClassyHubInterface
 from classy_vision.models import ClassyVisionModel, build_model
 from classy_vision.tasks import ClassyVisionTask, build_task
-from torchvision import transforms
+from torchvision import models, transforms
 
 
 class TestClassyHubInterface(unittest.TestCase):
@@ -29,7 +29,29 @@ class TestClassyHubInterface(unittest.TestCase):
         # delete all the temporary data created
         shutil.rmtree(self.base_dir)
 
-    def test_interface_with_task(self):
+    def _test_predict_and_extract_features(self, hub_interface: ClassyHubInterface):
+        dataset = hub_interface.create_image_dataset([self.image_path], split="test")
+        data_iterator = hub_interface.get_data_iterator(dataset)
+        input = next(data_iterator)
+        # set the model to eval mode
+        hub_interface.eval()
+        output = hub_interface.predict(input)
+        self.assertIsNotNone(output)
+        # see the prediction for the input
+        hub_interface.predict(input).argmax().item()
+        # check extract features
+        output = hub_interface.extract_features(input)
+        self.assertIsNotNone(output)
+
+    def _get_classy_model(self):
+        config = get_test_task_config()
+        model_config = config["model"]
+        return build_model(model_config)
+
+    def _get_non_classy_model(self):
+        return models.resnet18(pretrained=False)
+
+    def test_from_task(self):
         config = get_test_task_config()
         args = get_test_args()
         task = build_task(config, args)
@@ -39,32 +61,14 @@ class TestClassyHubInterface(unittest.TestCase):
         self.assertIsInstance(hub_interface.model, ClassyVisionModel)
 
         # this will pick up the transform from the task's config
-        dataset = hub_interface.create_image_dataset([self.image_path], split="test")
-        data_iterator = hub_interface.get_data_iterator(dataset)
-        input = next(data_iterator)
-        # set the model to eval mode
-        hub_interface.eval()
-        output = hub_interface.predict(input)
-        self.assertIsNotNone(output)
-        # see the prediction for the input
-        hub_interface.predict(input).argmax().item()
+        self._test_predict_and_extract_features(hub_interface)
 
-    def test_interface_with_model(self):
-        config = get_test_task_config()
-        model_config = config["model"]
-        model = build_model(model_config)
-        hub_interface = ClassyHubInterface.from_model(model)
+    def test_from_model(self):
+        for model in [self._get_classy_model(), self._get_non_classy_model()]:
+            hub_interface = ClassyHubInterface.from_model(model)
 
-        self.assertIsNone(hub_interface.task)
-        self.assertIsInstance(hub_interface.model, ClassyVisionModel)
+            self.assertIsNone(hub_interface.task)
+            self.assertIsInstance(hub_interface.model, ClassyVisionModel)
 
-        # this will pick up the transform from imagenet
-        dataset = hub_interface.create_image_dataset([self.image_path], split="test")
-        data_iterator = hub_interface.get_data_iterator(dataset)
-        input = next(data_iterator)
-        # set the model to eval mode
-        hub_interface.eval()
-        output = hub_interface.predict(input)
-        self.assertIsNotNone(output)
-        # see the prediction for the input
-        hub_interface.predict(input).argmax().item()
+            # this will pick up the transform from imagenet
+            self._test_predict_and_extract_features(hub_interface)
