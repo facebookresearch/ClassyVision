044ec92735fc3506a91c1f93c2378d011730c03f
Simran Suresh Motwani <simranmotwani@fb.com>
2019-10-07 16:20:38 -0700

    Move FullyConnectedLayer from resnext into classy_vision/heads (#41)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/41
    
    1. Moved FC head from resnext.py to classy_vision/heads
    2. Changed corresponding imports
    3. Changed head name to "fully_connected" and corresponding json files
    
    Reviewed By: mannatsingh, vreis
    
    Differential Revision: D17730080
    
    fbshipit-source-id: d346fdf59e3fc0aa2338efcb12c2c0c35337cd9e

--- classy_vision/models/resnext.py
+++ classy_vision/models/resnext.py
@@ -12,7 +12,6 @@ import math
 
 import torch.nn as nn
 from classy_vision.generic.util import is_pos_int
-from classy_vision.heads import ClassyVisionHead, register_head
 
 from . import register_model
 from .classy_vision_model import ClassyVisionModel
@@ -186,36 +185,6 @@ class BottleneckLayer(GenericLayer):
             reduction=reduction,
             final_bn_relu=final_bn_relu,
         )
-
-
-@register_head("resnext_fc")
-class FullyConnectedLayer(ClassyVisionHead):
-    def __init__(self, config):
-        super().__init__(config)
-        config = self.parse_config(config)
-        num_classes = config["num_classes"]
-        in_plane = config["in_plane"]
-        assert num_classes is None or is_pos_int(num_classes)
-        assert is_pos_int(in_plane)
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = None if num_classes is None else nn.Linear(in_plane, num_classes)
-
-    def parse_config(self, config):
-        assert "in_plane" in config
-        return {
-            "num_classes": config["num_classes"] if "num_classes" in config else None,
-            "in_plane": config["in_plane"],
-        }
-
-    def forward(self, x):
-        # perform average pooling:
-        out = self.avgpool(x)
-
-        # final classifier:
-        out = out.reshape(out.size(0), -1)
-        if self.fc is not None:
-            out = self.fc(out)
-        return out
 
 
 class SmallInputInitialBlock(nn.Module):
