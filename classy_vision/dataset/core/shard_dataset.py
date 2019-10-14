be0988df49a8d8bee0be91cda129b1899437a0e4
Zhicheng Yan <zyan3@fb.com>
2019-10-14 08:51:07 -0700

    support group size in ShardDataset (#54)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/54
    
    For video model evaluation, we sample N clips from a video, and average clip predictions to get a video-level prediction.
    Assume, we sample 2 clips per video. The test dataset, which has 4 videos {A,B,C,D} is illustrated below.
    
        [A_0, A_1, B_0, B_1, C_0, C_1, D_0, D_1]
    
    Assume we have 2 gpus. The existing ShardDataset will distribute clips from the same video to different gpus, and make it difficult to average clip predictions.
        GPU 0: [A_0, B_0, C_0, D_0]
        GPU 1: [A_1, B_1, C_1, D_1]
    
    This is called Stride Shading.
    
    We extend ShardDataset to support Sequential Sharding, which will shard clips below.
        GPU 0: [A_0, A_1, B_0, B_1]
        GPU 1: [C_0, C_1, D_0, D_1]
    
    This facilitates the averaging of clip predictions.
    
    Reviewed By: mannatsingh
    
    Differential Revision: D17662575
    
    fbshipit-source-id: 1639e9b98a6147a0a19399d6de764f1566852a08

--- classy_vision/dataset/core/shard_dataset.py
+++ classy_vision/dataset/core/shard_dataset.py
@@ -22,20 +22,32 @@ class ShardDataset(Dataset):
         Meters and loss from dummy_sample will be ignored.
 
         Sharding logic:
-        Sharding is done by going over the original dataset by taking `mod` operation
-        (detailed example below).
-        For incomplete shards, roll over the real samples to generate dummy samples.
+            Dataset is first divided into groups of samples. Sharding is done by
+            going over the original dataset by taking `mod` operation to sample
+            groups.  (detailed example below). For incomplete shards, roll over
+            the real samples to generate dummy samples.
 
-        Example:
-            dataset: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-            world_size: 4
+            Example:
+                dataset: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+                world_size: 4
 
-            RANK    |  shard_dataset  | is_dummy_sample
-            ===========================================
-            rank_0  |  [0, 4, 8, 12]  |  [0, 0, 0, 0]
-            rank_1  |  [1, 5, 9, 13]  |  [0, 0, 0, 0]
-            rank_2  |  [2, 6, 10, 2]  |  [0, 0, 0, 1]
-            rank_3  |  [3, 7, 11, 3]  |  [0, 0, 0, 1]
+            when group_size = 1
+                    RANK    |  shard_dataset  | is_dummy_sample
+                    ===========================================
+                    rank_0  |  [0, 4, 8, 12]  |  [0, 0, 0, 0]
+                    rank_1  |  [1, 5, 9, 13]  |  [0, 0, 0, 0]
+                    rank_2  |  [2, 6, 10, 2]  |  [0, 0, 0, 1]
+                    rank_3  |  [3, 7, 11, 3]  |  [0, 0, 0, 1]
+
+            when group_size = 2
+
+                    RANK    |  shard_dataset  | is_dummy_sample
+                    ===========================================
+                    rank_0  |  [0, 1, 8, 9]  |  [0, 0, 0, 0]
+                    rank_1  |  [2, 3, 10, 11]  |  [0, 0, 0, 0]
+                    rank_2  |  [4, 5, 4, 5]  |  [0, 0, 1, 1]
+                    rank_3  |  [6, 7, 6, 7]  |  [0, 0, 1, 1]
+
 
         Note:
             shard_dataset operates only on dataset with individual samples and
@@ -43,13 +55,20 @@ class ShardDataset(Dataset):
             .batch() call on dataset must happen after .shard() call.
     """
 
-    def __init__(self, dataset, world_size, rank):
+    def __init__(self, dataset, world_size, rank, group_size=1):
+        assert len(dataset) % group_size == 0, (
+            "dataset length must be a multiplier of group size"
+            "dataset length: %d, group size: %d" % (len(dataset), group_size)
+        )
         self.world_size = world_size
         self.rank = rank
         self.dataset = dataset
-        self.real_shard_length = (len(self.dataset) // self.world_size) + (
-            self.rank < (len(self.dataset) % self.world_size)
-        )
+        self.group_size = group_size
+        dataset_group_size = len(self.dataset) // group_size
+        self.real_shard_length = (
+            (dataset_group_size // self.world_size)
+            + (self.rank < (dataset_group_size % self.world_size))
+        ) * self.group_size
 
     def __getitem__(self, idx):
         # For last shard, loop over the real samples to generate dummy samples.
@@ -58,7 +77,13 @@ class ShardDataset(Dataset):
             is_dummy_sample = 1
         is_dummy_sample = torch.tensor(is_dummy_sample)
         idx = idx % self.real_shard_length
-        dataset_index = int(idx * self.world_size + self.rank)
+        group_idx = idx // self.group_size
+        group_remainder = idx % self.group_size
+        dataset_index = (
+            int(group_idx * self.world_size + self.rank) * self.group_size
+            + group_remainder
+        )
+
         sample = self.dataset[dataset_index]
         assert sample is None or isinstance(
             sample, dict
@@ -69,4 +94,5 @@ class ShardDataset(Dataset):
         return sample
 
     def __len__(self):
-        return int(math.ceil(len(self.dataset) * 1.0 / self.world_size))
+        dataset_group_size = len(self.dataset) // self.group_size
+        return int(math.ceil(dataset_group_size / self.world_size)) * self.group_size
