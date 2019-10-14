9d5b20a2fdad83a232362084f406e28f3ecab6b4
Zhicheng Yan <zyan3@fb.com>
2019-10-14 00:25:15 -0700

    support group size in ShardDataset (#54)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/54
    
    For video model evaluation, we sample N clips from a video, and average clip predictions to get a video-level prediction.
    Assume, we sample 2 clips per video. The test dataset, which has 4 videos {A,B,C,D} is illustrated below.
    
        [A_0, A_1, B_0, B_1, C_0, C_1, D_0, D_1]
    
    Assume we have 2 gpus. The existing ShardDataset will distribute clips from the same video to different gpus, and make it difficult to average clip predictions.
        GPU 0: [A_0, B_0, C_0, D_0]
        GPU 1: [A_1, B_1, C_1, D_1]
    
    This is called Stride Shading.
    
    We extend ShardDataset to support Sequential Sharding, which will shard clips below.
        GPU 0: [A_0, A_1, B_0, B_1]
        GPU 1: [C_0, C_1, D_0, D_1]
    
    This facilitates the averaging of clip predictions.
    
    Reviewed By: mannatsingh
    
    Differential Revision: D17662575
    
    fbshipit-source-id: 4a7a16a0cad17b9713ae120b9533c1fbd495082e

--- test/dataset_core_shard_dataset_test.py
+++ test/dataset_core_shard_dataset_test.py
@@ -83,3 +83,88 @@ class ShardDatasetTest(unittest.TestCase):
 
         # Check that last two samples are dummy samples.
         self.assertTrue(shard_dataset[3]["is_dummy_sample"] == torch.tensor(1))
+
+    def test_shard_dataset_length_group_size(self):
+        """
+        Test that sharded dataset length is correct.
+        """
+        test_dataset, _ = create_test_dataset(tensor_size=(260, 3, 224, 224))
+        WORLD_SIZE = 8
+
+        for rank in range(WORLD_SIZE):
+            shard_dataset = ShardDataset(
+                test_dataset, world_size=WORLD_SIZE, rank=rank, group_size=10
+            )
+            self.assertTrue(len(shard_dataset) == 40)
+
+    def test_non_final_shard_dataset_index_group_size(self):
+        """
+        Test that for non-final shard, the indices map to
+        correct indices from the main dataset.
+        """
+        test_dataset, _ = create_test_dataset(tensor_size=(260, 3, 224, 224))
+        WORLD_SIZE = 8
+        RANK = 1
+
+        shard_dataset = ShardDataset(
+            test_dataset, world_size=WORLD_SIZE, rank=RANK, group_size=10
+        )
+
+        for i in range(len(shard_dataset)):
+            # Check if video clip is correct.
+            group_idx = i // 10
+            group_reminder = i % 10
+            ind = (group_idx * WORLD_SIZE + RANK) * 10 + group_reminder
+            self.assertTrue(
+                torch.all(
+                    shard_dataset[i]["input"] == test_dataset[ind]["input"]
+                ).item()
+                == 1
+            )
+            # Check if target is correct
+            self.assertTrue(
+                torch.all(
+                    shard_dataset[i]["target"] == test_dataset[ind]["target"]
+                ).item()
+                == 1
+            )
+            # For rank 0 and 1, all 40 samples are real
+            self.assertTrue(shard_dataset[i]["is_dummy_sample"] == torch.tensor(0))
+
+    def test_final_shard_dataset_index_sequential_mode(self):
+        """
+        Test that for final shard, the indices map to correct indices from the
+        main dataset and is_dummy_sample is as expected.
+        """
+        test_dataset, _ = create_test_dataset(tensor_size=(260, 3, 224, 224))
+        WORLD_SIZE = 8
+        RANK = 4
+
+        shard_dataset = ShardDataset(
+            test_dataset, world_size=WORLD_SIZE, rank=RANK, group_size=10
+        )
+        # Check that for rank 2, 3, 4, 5, 6, 7, first 30 samples are real and
+        # last 10 samples are dummy.
+        for i in range(30):
+            # Check if video clip is correct.
+            group_idx = i // 10
+            group_reminder = i % 10
+            ind = (group_idx * WORLD_SIZE + RANK) * 10 + group_reminder
+            self.assertTrue(
+                torch.all(
+                    shard_dataset[i]["input"] == test_dataset[ind]["input"]
+                ).item()
+                == 1
+            )
+            # Check if target is correct
+            self.assertTrue(
+                torch.all(
+                    shard_dataset[i]["target"] == test_dataset[ind]["target"]
+                ).item()
+                == 1
+            )
+            self.assertTrue(shard_dataset[i]["is_dummy_sample"] == torch.tensor(0))
+
+        for i in range(30, 40):
+            # Check that last 10 samples are dummy samples.
+            self.assertTrue(shard_dataset[i]["is_dummy_sample"] == torch.tensor(1))
