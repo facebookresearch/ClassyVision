8a6afebb5c6c8177eb715f1973f5ad213d725c1a
Zhicheng Yan <zyan3@fb.com>
2019-10-15 13:12:40 -0700

    extend profiler and visualizer to support dict-type input in model forward method (#60)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/60
    
    In `forward` method of video models, we take input of data type dictionary where key is modality such as 'video' and `audio`.
    To support this, we extend profiler and `plot_model` to prepare dictionary input as needed.
    
    Reviewed By: mannatsingh
    
    Differential Revision: D17770657
    
    fbshipit-source-id: 723f164f5359a60b9208689d7a8545b8f9909287

--- classy_vision/generic/util.py
+++ classy_vision/generic/util.py
@@ -696,3 +696,14 @@ def bind_method_to_class(method, cls):
     Binds an already bound method to the provided class.
     """
     return method.__func__.__get__(cls)
+
+
+def get_model_dummy_input(model, input_shape, input_key):
+    # add a dimension to represent minibatch axis
+    shape = (1,) + tuple(input_shape)
+    input = torch.zeros(shape)
+    if next(model.parameters()).is_cuda:
+        input = input.cuda()
+    if input_key:
+        input = {input_key: input}
+    return input
