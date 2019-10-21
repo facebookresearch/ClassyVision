c5084be705cf180534a5dcbe535d51d6ac9f70a5
Aaron Adcock <aadcock@fb.com>
2019-10-21 10:48:14 -0700

    Isolate Everstore datasets and remove batch wrapper (#87)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/87
    
    This diff has the goal of removing the custom batching logic from the classy datasets and instead directly using the PyTorch DataLoader's batching logic.
    
    It became slightly more complicated because there are several blockers to migrating the EverstoreDatasets to the PyTorch DataLoaders (mainly backfilling logic and my attempts to serialize the EverstoreDataset have repeatedly run into issues).
    
    So, instead, I do the following:
    
    1. Isolate the EverstoreDataset with its own ClassyEverstoreDataset class. This class still uses the BatchDataset, etc. and the Classy Dataloader logic, etc. Goal is to migrate the custom code to the FB folder / isolate the usage in the EverstoreDatasets so that it can all be deleted once we migrate all users to Koski datasets.
    2. Move OSS batching to PyTorch Dataloader.
    
    At the end of this stack I want all dataset wrappers to be deleted or in the fb folder, the Dataset object will be deleted, and I will move the everstore functions to a folder marked as deprecated.
    
    NOTE: I'm aware I conflated two changes in this diff. I thought I had successfully isolated the everstore and then realized afterwards that I needed a separate class and it's a bit annoying to split the diff. That being said, I'm happy to split the diff if it's difficult to review.
    
    Reviewed By: mannatsingh
    
    Differential Revision: D17896229
    
    fbshipit-source-id: 8a3b99b63634e0e6ac9827121ac75cf387555aa6

--- classy_vision/dataset/core/__init__.py
+++ classy_vision/dataset/core/__init__.py
@@ -4,7 +4,6 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .batch_dataset import BatchDataset
 from .dataset import Dataset
 from .list_dataset import ListDataset
 from .random_image_datasets import RandomImageBinaryClassDataset, RandomImageDataset
@@ -16,11 +15,7 @@ from .wrap_dataset import WrapDataset
 from .wrap_torchvision_video_dataset import WrapTorchVisionVideoDataset
 
 
-# TODO: Fix this:
-# from .pairwise_sampler import PairwiseSampler, PairwiseBatchSampler
-
 __all__ = [
-    "BatchDataset",
     "Dataset",
     "ListDataset",
     "RandomImageBinaryClassDataset",
