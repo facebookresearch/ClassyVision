461920c0c16ac9fc03690350a5cd7876a27f300c
Vinicius Reis <vini@fb.com>
2019-11-01 15:43:49 -0700

    ClassyHead documentation
    Summary: Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/160
    
    Differential Revision: D18274974
    
    Pulled By: vreis
    
    fbshipit-source-id: e6f7549866c6a8d43df02968fa0c399d4ad0f70b

--- classy_vision/heads/classy_head.py
+++ classy_vision/heads/classy_head.py
@@ -10,12 +10,28 @@ import torch.nn as nn
 
 
 class ClassyHead(nn.Module):
+    """
+    Base class for heads that can be attached to :class:`ClassyModel`.
+
+    A head is a regular :class:`torch.nn.Module` that can be attached to a
+    pretrained model. This enables a form of transfer learning: utilizing a
+    model trained for one dataset to extract features that can be used for
+    other problems. A head must be attached to a :class:`ClassyModule` within a
+    :class:`ClassyModel`.
+    """
+
     def __init__(
         self, unique_id: Optional[str] = None, num_classes: Optional[int] = None
     ):
         """
-        Classy Head constructor.
-        This is the place to build and initialize the layers.
+        Constructs a ClassyHead.
+
+        Args:
+            unique_id: A unique identifier for the head. Multiple instances of
+                the same head might be attached to a model, and unique_id is used
+                to refer to them.
+
+            num_classes: Number of classes for the head.
         """
         super().__init__()
         self.unique_id = unique_id or self.__class__.__name__
@@ -26,4 +42,10 @@ class ClassyHead(nn.Module):
         raise NotImplementedError
 
     def forward(self, x):
+        """
+        Performs inference on the head.
+
+        This is a regular PyTorch method, refer to :class:`torch.nn.Module` for
+        more details
+        """
         raise NotImplementedError
