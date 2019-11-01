34a8270b7a88967f66438bd9bfa590c00f661a55
Aaron Adcock <aadcock@fb.com>
2019-11-01 14:49:16 -0700

    Torchhub documentation (#26)
    Summary:
    Pull Request resolved: https://github.com/fairinternal/ClassyVision/pull/26
    
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/161
    
    Torchhub documentation via sphinx
    
    Reviewed By: vreis
    
    Differential Revision: D18274443
    
    fbshipit-source-id: b7d58839d154e753b548666969fd313085f5cd62

--- classy_vision/hub/classy_hub_interface.py
+++ classy_vision/hub/classy_hub_interface.py
@@ -16,16 +16,37 @@ from classy_vision.tasks import ClassyTask
 
 
 class ClassyHubInterface:
-    """
-    PyTorch Hub interface to classy vision tasks and models.
+    """PyTorch Hub interface for classy vision tasks and models.
 
-    The task is optional, but a model is guaranteed to be present.
-    Use from_task() or from_model() to instantiate the class.
+    The task is optional, but a model is guaranteed to be present.  Do
+    not use the constructor directly, instead Use from_task() or
+    from_model() to instantiate the class.
+
+    See the examples folder for an example of how to use this class
+
+    Attributes:
+        task: If present, task that can be used to train the torchhub model
+        model: torchub model
+
     """
 
     def __init__(
         self, task: Optional[ClassyTask] = None, model: Optional[ClassyModel] = None
     ) -> None:
+        """Constructor for ClassyHubInterface.
+
+        Only one of task or model can be specified at construction
+        time. If task is specified then task.model is used to populate
+        the model attribute.
+
+        Do not use the constructor directly, instead use from_task()
+        or from_model() to instantiate the class.
+
+        Args:
+            task: task that can be used to train torchhub model,
+                task.model is used to populate the model attribute
+            model: torchhub model
+        """
         self.task = task
         if task is None:
             assert model is not None, "Need to specify a model if task is None"
@@ -36,10 +57,26 @@ class ClassyHubInterface:
 
     @classmethod
     def from_task(cls, task: ClassyTask) -> "ClassyHubInterface":
+        """Instantiates the ClassyHubInterface from a task.
+
+        This function returns a hub interface based on a ClassyTask.
+
+        Args:
+            task: ClassyTask that contains hub model
+
+        """
         return cls(task=task)
 
     @classmethod
     def from_model(cls, model: Union[nn.Module, ClassyModel]) -> "ClassyHubInterface":
+        """Instantiates the ClassyHubInterface from a model.
+
+        This function returns a hub interface based on a ClassyModel
+
+        Args:
+            model: torchhub model
+
+        """
         if not isinstance(model, ClassyModel):
             model = ClassyModelWrapper(model)
         return cls(model=model)
@@ -54,21 +91,27 @@ class ClassyHubInterface:
         num_samples: Optional[int] = None,
         split: str = "train",
     ) -> ClassyDataset:
-        """
-        Create a ClassyDataset which reads images from image_paths.
+        """Create a ClassyDataset which reads images from image_paths.
 
-        image_paths: Can be
-            - A single directory location, in which case the data is expected to be
-                arranged in a format similar to torchvision.datasets.ImageFolder.
-                The targets will be inferred from the directory structure.
-            - A list of paths, in which case the list will contain the paths to all the
-                images. In this situation, the targets can be specified by the targets
-                argument.
-        targets (optional): A list containing the target classes for each image
-        transform (optional): Transform to apply. If left as None, the dataset's
-            split is used to determine the transform to apply. The transform for the
-            split is searched for in self.task, falling back to imagenet transformations
-            if it is not found there.
+        Args:
+            image_paths: Can be
+                - A single directory location, in which case the data is expected to be
+                    arranged in a format similar to
+                    `:class:torchvision.datasets.ImageFolder`. The targets will
+                    be inferred from the directory structure.
+                - A list of paths, in which case the list will contain the paths
+                    to all the images. In this situation, the targets can be specified
+                    by using the targets argument.
+            targets: A list containing the target classes for each image
+            batchsize_per_replica: Minibatch size per replica (i.e. samples per GPU)
+            shuffle: If true, data is shuffled between epochs
+            transform: Transform to apply to sample. If left as None, the dataset's
+                split is used to determine the transform to apply. The transform for the
+                split is searched for in self.task, falling back to imagenet
+                transformations if it is not found there.
+            num_samples: If specified, limits the number of samples returned by
+                the dataset
+            split: String specifying the split of data used, e.g. "train" or "test"
         """
         if transform is None:
             if self.task is not None and split in self.task.datasets:
@@ -92,28 +135,43 @@ class ClassyHubInterface:
 
     @staticmethod
     def get_data_iterator(dataset: ClassyDataset) -> Iterator[Any]:
+        """Returns an iterator that can be used to retrieve training / testing samples.
+
+        Args:
+            dataset: Dataset to iterate over
+        """
         return iter(dataset.iterator())
 
     def train(self) -> None:
-        """
-        Sets the model to train mode and enables torch gradient calculation
+        """Sets the model to train mode and enables torch gradient calculation
+
         """
         torch.autograd.set_grad_enabled(True)
         self.model.train()
 
     def eval(self) -> None:
-        """
-        Sets the model to eval mode and disables torch gradient calculation
+        """Sets the model to eval mode and disables torch gradient calculation
+
         """
         torch.autograd.set_grad_enabled(False)
         self.model.eval()
 
     def predict(self, sample):
+        """Returns the model's prediction for a sample.
+
+        Args:
+            sample: Must contain "input" key, model calculates prediction over input.
+        """
         output = self.model(sample["input"])
         # squeeze the output in case the batch size is 1
         return output.squeeze()
 
     def extract_features(self, sample):
+        """Calculates feature embeddings of sample.
+
+        Args:
+            sample: Must contain "input" key, model calculates prediction over input.
+        """
         output = self.model.extract_features(sample["input"])
         # squeeze the output in case the batch size is 1
         return output.squeeze()
