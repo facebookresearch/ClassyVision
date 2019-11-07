b11c3a31b94f08a111f57b1cc517636704d8ca4d
Vinicius Reis <vini@fb.com>
2019-11-07 11:17:25 -0800

    Utility to start a classy project (#152)
    Summary:
    Adds a utility to start a new project with classy vision. This gets installed as a binary under /bin and can be invoked anywhere. Usage is
    
      classy-project <project_name>
    
    This creates a folder called project_name with a few template files and a version of classy_train.py.
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/152
    
    Test Plan: New integration test
    
    Reviewed By: mannatsingh
    
    Differential Revision: D18228965
    
    Pulled By: vreis
    
    fbshipit-source-id: f64cbf8f5fa4fd70f2f0e7ac224cd5622e2ff577

---
+++ classy_vision/templates/synthetic/models/my_model.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import torchvision.models as models
+from classy_vision.models import ClassyModel, register_model
+
+
+@register_model("my_model")
+class MyModel(ClassyModel):
+    def __init__(self):
+        super().__init__(2)
+        self.resnet = models.resnet18()
+
+    def forward(self, x):
+        return self.resnet(x)
+
+    @classmethod
+    def from_config(cls, config):
+        return cls()
