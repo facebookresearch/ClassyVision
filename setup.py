1c780955afc0e9a718525deeae44a5dce58536e3
Vinicius Reis <vini@fb.com>
2019-11-16 10:59:58 -0800

    Improve setup.py
    Summary: Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/221
    
    Test Plan: .
    
    Differential Revision: D18533056
    
    Pulled By: vreis
    
    fbshipit-source-id: 889d0cc6b0c99f17d11a7b38391fb99754e4dc4a

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@
 # LICENSE file in the root directory of this source tree.
 
 
+import os
+import re
 import sys
 
 from setuptools import find_packages, setup
@@ -13,6 +15,15 @@ from setuptools import find_packages, setup
 if __name__ == "__main__":
     if sys.version_info < (3, 6):
         sys.exit("Sorry, Python >=3.6 is required for Classy Vision.")
+
+    # get version string from module
+    with open(
+        os.path.join(os.path.dirname(__file__), "classy_vision/__init__.py"), "r"
+    ) as f:
+        version = re.search(r"__version__ = ['\"]([^'\"]*)['\"]", f.read(), re.M).group(
+            1
+        )
+        print("-- Building version " + version)
 
     with open("README.md", encoding="utf8") as f:
         readme = f.read()
@@ -24,18 +35,34 @@ if __name__ == "__main__":
         reqs = f.read()
 
     setup(
-        name="ClassyVision",
-        version="0.1.0",
+        name="classy_vision",
+        version=version,
         description="An end-to-end computer vision framework.",
         long_description=readme,
-        url="TBD",
+        url="https://classyvision.ai",
+        project_urls={
+            "Documentation": "https://classyvision.ai",
+            "Source": "https://github.com/facebookresearch/ClassyVision",
+        },
         license=license,
         python_requires=">=3.6",
         packages=find_packages(exclude=("tests",)),
         install_requires=reqs.strip().split("\n"),
+        extras_require={"dev": ["black", "sphinx", "isort", "bs4", "nbconvert"]},
         package_data={"classy_vision": ["configs/*.json", "templates"]},
         data_files=[("classy_vision", ["classy_train.py"])],
         include_package_data=True,
         test_suite="test.suites.unittests",
         scripts=["bin/classy-project"],
+        keywords=["deep learning", "pytorch", "AI"],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.6",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "Topic :: Scientific/Engineering :: Artificial Intelligence",
+            "Topic :: Scientific/Engineering :: Image Recognition",
+        ],
     )
