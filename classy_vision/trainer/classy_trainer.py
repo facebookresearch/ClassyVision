3d38367bc78b03d570c1aeaaab8f12ba3844aa96
Aaron Adcock <aadcock@fb.com>
2019-11-17 20:29:13 -0800

    Trainer Docs (#211)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/211
    
    Pull Request resolved: https://github.com/fairinternal/ClassyVision/pull/38
    
    Docs for our Classy OSS Trainers, also added type hints
    
    Reviewed By: vreis
    
    Differential Revision: D18496512
    
    fbshipit-source-id: 40095abfc9db54b0c129de9dff585cbe68fd5d3e

--- classy_vision/trainer/classy_trainer.py
+++ classy_vision/trainer/classy_trainer.py
@@ -5,6 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 import logging
+from typing import Optional
 
 import torch
 from classy_vision.generic.distributed_util import barrier, is_distributed_training_run
@@ -13,9 +14,37 @@ from classy_vision.tasks import ClassyTask
 
 
 class ClassyTrainer:
+    """Base class for shared training code.
+
+    A trainer is responsible for setting up the environment for
+    training, for instance: configuring rendezvous for distributed
+    training, deciding what GPU to use and so on. Trainers also
+    control the outer portion of the training loop, but delegate to
+    the task to decide how exactly to perform inference, compute loss
+    etc. That allows combining tasks with different trainers depending
+    on whether you want to train on your current machine, AWS cluster
+    etc.
+
+    """
+
     def __init__(
-        self, use_gpu=None, num_dataloader_workers=0, dataloader_mp_context=None
+        self,
+        use_gpu: Optional[bool] = None,
+        num_dataloader_workers: int = 0,
+        dataloader_mp_context: Optional[str] = None,
     ):
+        """Constructor for ClassyTrainer.
+
+        Args:
+            use_gpu: If true, then use GPUs for training.
+                If None, then check if we have GPUs available, if we do
+                then use GPU for training.
+            num_dataloader_workers: Number of CPU processes doing dataloading
+                per GPU. If 0, then dataloading is done on main thread.
+            dataloader_mp_context: Determines how to launch
+                new processes for dataloading. Must be one of "fork", "forkserver",
+                "spawn". If None, process launching is inherited from parent.
+        """
         if use_gpu is None:
             use_gpu = torch.cuda.is_available()
         self.use_gpu = use_gpu
@@ -23,8 +52,11 @@ class ClassyTrainer:
         self.dataloader_mp_context = dataloader_mp_context
 
     def train(self, task: ClassyTask):
-        """
-        Runs training phases, phases are generated from the config.
+        """Runs training phases, phases are generated from the config.
+
+        Args:
+            task: Task to be used in training. It should contain
+                everything that is needed for training
         """
 
         pin_memory = self.use_gpu and torch.cuda.device_count() > 1
--- classy_vision/trainer/local_trainer.py
+++ classy_vision/trainer/local_trainer.py
@@ -5,6 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 import logging
+from typing import Optional
 
 from classy_vision.generic.distributed_util import set_cpu_device, set_cuda_device_index
 
@@ -12,9 +13,27 @@ from .classy_trainer import ClassyTrainer
 
 
 class LocalTrainer(ClassyTrainer):
+    """Trainer to be used if you want want use only a single training process.
+    """
+
     def __init__(
-        self, use_gpu=None, num_dataloader_workers=0, dataloader_mp_context=None
+        self,
+        use_gpu: Optional[bool] = None,
+        num_dataloader_workers: int = 0,
+        dataloader_mp_context: Optional[str] = None,
     ):
+        """Constructor for LocalTrainer.
+
+        Args:
+            use_gpu: If true, then use GPU 0 for training.
+                If None, then check if we have GPUs available, if we do
+                then use GPU for training.
+            num_dataloader_workers: Number of CPU processes doing dataloading
+                per GPU. If 0, then dataloading is done on main thread.
+            dataloader_mp_context: Determines how to launch
+                new processes for dataloading. Must be one of "fork", "forkserver",
+                "spawn". If None, process launching is inherited from parent.
+        """
         super().__init__(
             use_gpu=use_gpu,
             num_dataloader_workers=num_dataloader_workers,
