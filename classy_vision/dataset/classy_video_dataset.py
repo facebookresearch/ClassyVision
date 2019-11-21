62bc2c8444dadd29b834a877a71a4dbc88478aca
Zhicheng Yan <zyan3@fb.com>
2019-11-20 20:49:25 -0800

    docs for ClassyVideoDataset (#234)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/234
    
    Docs for OSS `ClassyVideoDataset` class and other related classes.
    
    Reviewed By: aadcock
    
    Differential Revision: D18584424
    
    fbshipit-source-id: bebe4a0abd3ac00a3077593692488db5843e23ca

--- classy_vision/dataset/classy_video_dataset.py
+++ classy_vision/dataset/classy_video_dataset.py
@@ -7,6 +7,7 @@
 import logging
 import multiprocessing
 import os
+from typing import Any, Callable, Dict, Optional
 
 import torch
 from classy_vision.generic.distributed_util import get_rank, get_world_size
@@ -21,18 +22,21 @@ from .classy_dataset import ClassyDataset
 
 
 class MaxLengthClipSampler(Sampler):
-    """MaxLengthClipSampler is a thin wrapper on top of clip samplers in TorchVision
-        It takes as input a TorchVision clip sampler, and an optional argument
-        `num_samples` to limit the number of samples.
+    """MaxLengthClipSampler is a thin wrapper on top of clip samplers in TorchVision.
+
+    It takes as input a TorchVision clip sampler, and an optional argument
+    `num_samples` to limit the number of samples.
     """
 
     def __init__(self, clip_sampler, num_samples=None):
-        """
+        """The constructor method of MaxLengthClipSampler.
+
         Args:
             clip_sampler: clip sampler without a limit on the total number of clips
                 it can sample, such as RandomClipSampler and UniformClipSampler.
             num_samples: if provided, it denotes the maximal number of clips the sampler
                 will return
+
         """
         self.clip_sampler = clip_sampler
         self.num_samples = num_samples
@@ -56,20 +60,34 @@ class MaxLengthClipSampler(Sampler):
 
 
 class ClassyVideoDataset(ClassyDataset):
-    """
-    Interface specifying what a Classy Vision video dataset can be expected to provide.
+    """Interface specifying what a ClassyVision video dataset is expected to provide.
     """
 
     def __init__(
         self,
-        dataset,
-        split,
-        batchsize_per_replica,
-        shuffle,
-        transform,
-        num_samples,
-        clips_per_video,
+        dataset: Any,
+        split: str,
+        batchsize_per_replica: int,
+        shuffle: bool,
+        transform: Callable,
+        num_samples: Optional[int],
+        clips_per_video: int,
     ):
+        """The constructor method of ClassyVideoDataset.
+
+        Args:
+            dataset: the underlying video dataset from either TorchVision or other
+                source. It should have an attribute `video_clips` of type
+                torchvision.datasets.video_utils.VideoClips
+            split: dataset split. Must be either "train" or "test"
+            batchsize_per_replica: batch size per model replica
+            shuffle: If true, shuffle video clips.
+            transform: callable function to transform video clip sample from
+                ClassyVideoDataset
+            num_samples: If provided, return at most `num_samples` video clips
+            clips_per_video: The number of clips sampled from each video
+
+        """
         super(ClassyVideoDataset, self).__init__(
             dataset, split, batchsize_per_replica, shuffle, transform, num_samples
         )
@@ -77,7 +95,8 @@ class ClassyVideoDataset(ClassyDataset):
         self.clips_per_video = clips_per_video
 
     @classmethod
-    def parse_config(cls, config):
+    def parse_config(cls, config: Dict[str, Any]):
+        """Parse config to prepare arguments needed by the class constructor."""
         assert "frames_per_clip" in config, "frames_per_clip must be set"
 
         video_width = config.get("video_width", 0)
@@ -122,7 +141,35 @@ class ClassyVideoDataset(ClassyDataset):
         )
 
     @classmethod
-    def load_metadata(cls, filepath, video_dir=None, update_file_path=False):
+    def load_metadata(
+        cls,
+        filepath: str,
+        video_dir: Optional[str] = None,
+        update_file_path: bool = False,
+    ) -> Dict[str, Any]:
+        """Load pre-computed video dataset meta data.
+
+        Video dataset meta data computation takes minutes on small dataset and hours
+        on large dataset, and thus is time-consuming. However, it only needs to be
+        computed once, and can be saved into file. Later we can load the meta data
+        to reuse it.
+
+        The format of meta data is defined in TorchVision as shown below.
+        https://github.com/pytorch/vision/blob/master/torchvision/datasets/
+        video_utils.py#L131
+
+        For each video, meta data contains the video file path, presentation
+        timestamps of all video frames, and video fps.
+
+        Args:
+            filepath: file path of pre-computed meta data
+            video_dir: If provided, the folder where video files are stored.
+            update_file_path: If true, replace the directory part of video file path
+                in meta data with the actual video directory provided in `video_dir`.
+                This is needed for successsfully reusing pre-computed meta data
+                when video directory has been moved and it is no longer consitent
+                with the full video file path saved in the meta data.
+        """
         metadata = torch.load(filepath)
         if video_dir is not None and update_file_path:
             # video path in meta data can be computed in a different root video folder
@@ -138,7 +185,20 @@ class ClassyVideoDataset(ClassyDataset):
         return metadata
 
     @classmethod
-    def save_metadata(cls, metadata, filepath):
+    def save_metadata(cls, metadata: Dict[str, Any], filepath: str):
+        """Save dataset meta data into a file.
+
+        Args:
+            metadata: dataset meta data, which contains video meta infomration, such
+                as video file path, video fps, video frame timestamp in each video.
+                For the format of dataset meta data, check the TorchVision
+                documentations below.
+                https://github.com/pytorch/vision/blob/master/torchvision/datasets
+                /video_utils.py#L132-L137
+
+            filepath: file path where the meta data will be saved
+
+        """
         filedir = os.path.dirname(filepath)
         if not os.path.exists(filedir):
             try:
@@ -154,9 +214,14 @@ class ClassyVideoDataset(ClassyDataset):
 
     @property
     def video_clips(self):
+        """Attribute video_clips.
+
+        It is used in `_get_sampler` method. Its data type should be
+            torchvision.datasets.video_utils.VideoClips.
+        """
         return self.dataset.video_clips
 
-    def _get_sampler(self, epoch):
+    def _get_sampler(self, epoch) -> "DistributedSampler":
         if self.split == "train":
             # For video model training, we don't necessarily want to use all possible
             # clips in the video in one training epoch. More often, we randomly
@@ -180,10 +245,16 @@ class ClassyVideoDataset(ClassyDataset):
         return sampler
 
     def iterator(self, *args, **kwargs):
-        # for video dataset, it may use VideoClips class from TorchVision,
-        # which further use a cpp python extension for video decoding.
-        # It is difficult to use "spawning" as multiprocessing start method.
-        # Thus we choose "fork" as multiprocessing start method.
+        """Overrides the implementation in parent class `ClassyDataset`.
+
+        You can check all the usable positional and keyword arguments in parent
+        class `ClassyDataset.iterator(...)`.
+        For video dataset, it may use VideoClips class from TorchVision,
+        which may use a cpp python extension for video decoding when video backend
+        is set to `video_reader`. In such case, it is difficult to use "spawning"
+        as multiprocessing start method. Thus we choose "fork" as multiprocessing
+        start method.
+        """
         if "num_workers" in kwargs and kwargs["num_workers"] > 0:
             mp = multiprocessing.get_context("fork")
             kwargs["multiprocessing_context"] = mp
