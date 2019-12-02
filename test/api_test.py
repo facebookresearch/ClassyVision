cdaa22529b21516c36492f0f9e80f9d5593bfc6f
Mannat Singh <mannatsingh@fb.com>
2019-12-02 10:19:22 -0800

    Make MyDataset consistent with Imagenet (#256)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/256
    
    Made it so that MyDataset works the same way as the Imagenet dataset (returning a tuple as output).
    
    Reviewed By: vreis
    
    Differential Revision: D18718597
    
    fbshipit-source-id: ddabc8d58997bc3f246140c4d99d9488cf3eec89

--- test/api_test.py
+++ test/api_test.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates.
 #
 # This source code is licensed under the MIT license found in the
@@ -6,24 +7,79 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import unittest
+from typing import Any, Callable, Dict, Optional, Union
 
 import torch.nn as nn
 import torch.nn.functional as F
-from classy_vision.dataset import register_dataset
-from classy_vision.dataset.classy_synthetic_image import SyntheticImageDataset
-from classy_vision.dataset.transforms import ApplyTransformToKey
+from classy_vision.dataset import ClassyDataset, register_dataset
+from classy_vision.dataset.core.random_image_datasets import (
+    RandomImageBinaryClassDataset,
+    SampleType,
+)
+from classy_vision.dataset.transforms import (
+    ClassyTransform,
+    GenericImageTransform,
+    build_transforms,
+)
 from classy_vision.losses import ClassyLoss, register_loss
 from classy_vision.models import ClassyModel, register_model
 from classy_vision.optim import SGD
 from classy_vision.optim.param_scheduler import ConstantParamScheduler
 from classy_vision.tasks import ClassificationTask
 from classy_vision.trainer import LocalTrainer
-from torchvision.transforms import ToTensor
+from torchvision import transforms
 
 
 # WARNING: The goal of this test is to use our public API as advertised in our
 # tutorials and make sure everything trains successfully. If you break this
 # test, make sure you also update our tutorials.
+
+
+@register_dataset("my_dataset")
+class MyDataset(ClassyDataset):
+    def __init__(
+        self,
+        batchsize_per_replica: int,
+        shuffle: bool,
+        transform: Optional[Union[ClassyTransform, Callable]],
+        num_samples: int,
+        crop_size: int,
+        class_ratio: float,
+        seed: int,
+        split: Optional[str] = None,
+    ) -> None:
+        dataset = RandomImageBinaryClassDataset(
+            crop_size, class_ratio, num_samples, seed, SampleType.TUPLE
+        )
+        super().__init__(
+            dataset, split, batchsize_per_replica, shuffle, transform, num_samples
+        )
+
+    @classmethod
+    def from_config(cls, config: Dict[str, Any]) -> "MyDataset":
+        assert all(key in config for key in ["crop_size", "class_ratio", "seed"])
+
+        split = config.get("split")
+        crop_size = config["crop_size"]
+        class_ratio = config["class_ratio"]
+        seed = config["seed"]
+        (
+            transform_config,
+            batchsize_per_replica,
+            shuffle,
+            num_samples,
+        ) = cls.parse_config(config)
+        transform = build_transforms(transform_config)
+        return cls(
+            batchsize_per_replica,
+            shuffle,
+            transform,
+            num_samples,
+            crop_size,
+            class_ratio,
+            seed,
+            split=split,
+        )
 
 
 @register_loss("my_loss")
@@ -36,12 +92,6 @@ class MyLoss(ClassyLoss):
     def from_config(cls, config):
         # We don't need anything from the config
         return cls()
-
-
-@register_dataset("my_dataset")
-class MyDataset(SyntheticImageDataset):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
 
 
 @register_model("my_model")
@@ -69,7 +119,18 @@ class APITest(unittest.TestCase):
         train_dataset = MyDataset(
             batchsize_per_replica=32,
             shuffle=False,
-            transform=ApplyTransformToKey(ToTensor()),
+            transform=GenericImageTransform(
+                transform=transforms.Compose(
+                    [
+                        transforms.RandomResizedCrop(224),
+                        transforms.RandomHorizontalFlip(),
+                        transforms.ToTensor(),
+                        transforms.Normalize(
+                            mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                        ),
+                    ]
+                )
+            ),
             num_samples=100,
             crop_size=224,
             class_ratio=0.5,
@@ -79,7 +140,18 @@ class APITest(unittest.TestCase):
         test_dataset = MyDataset(
             batchsize_per_replica=32,
             shuffle=False,
-            transform=ApplyTransformToKey(ToTensor()),
+            transform=GenericImageTransform(
+                transform=transforms.Compose(
+                    [
+                        transforms.Resize(256),
+                        transforms.CenterCrop(224),
+                        transforms.ToTensor(),
+                        transforms.Normalize(
+                            mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                        ),
+                    ]
+                )
+            ),
             num_samples=100,
             crop_size=224,
             class_ratio=0.5,
