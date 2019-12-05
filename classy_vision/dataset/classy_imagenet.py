dc4bdd12f0760f424297b5e9f6dff3f22be9e37f
Mannat Singh <mannatsingh@fb.com>
2019-12-05 10:32:20 -0800

    OSS Imagenet dataset update (#275)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/275
    
    - Updated the OSS Imagenet dataset to remove the `download` arg. Instead, the dataset now uses `ImageFolder` and expects the user to extract the data in a compatible format.
    - Deleted `dataset_classy_oss_imagenet_test.py` (fb folder) and created `dataset_classy_imagenet_test.py` (oss  folder). This test writes test images to a dir and tests that the dataset can load them
    - Updated the imagenet configs to use `classy_imagenet` instead of `imagenet` and added the transforms
    - Added a short section in the getting started tutorial with instructions to train on imagenet and a 90 epoch config that reaches 76.2% accuracy
    
    Reviewed By: vreis
    
    Differential Revision: D18796349
    
    fbshipit-source-id: cb2d3b505f54ad33d21c6ade1c8a8ccf063337bb

--- classy_vision/dataset/classy_imagenet.py
+++ classy_vision/dataset/classy_imagenet.py
@@ -4,26 +4,27 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import os
 from typing import Any, Callable, Dict, Optional, Union
 
 from classy_vision.dataset import ClassyDataset, register_dataset
 from classy_vision.dataset.transforms import ClassyTransform, build_transforms
-from torchvision.datasets.imagenet import ImageNet
+from torchvision.datasets import ImageFolder
 
 
 @register_dataset("classy_imagenet")
 class ImageNetDataset(ClassyDataset):
     def __init__(
         self,
-        split: Optional[str],
+        split: str,
         batchsize_per_replica: int,
         shuffle: bool,
         transform: Optional[Union[ClassyTransform, Callable]],
         num_samples: Optional[int],
         root: str,
-        download: bool = None,
     ):
-        dataset = ImageNet(root=root, split=split, download=download)
+        img_dir = os.path.join(root, split)
+        dataset = ImageFolder(img_dir)
         super().__init__(
             dataset, split, batchsize_per_replica, shuffle, transform, num_samples
         )
@@ -47,7 +48,6 @@ class ImageNetDataset(ClassyDataset):
         ) = cls.parse_config(config)
         split = config.get("split")
         root = config.get("root")
-        download = config.get("download")
 
         transform = build_transforms(transform_config)
         return cls(
@@ -57,5 +57,4 @@ class ImageNetDataset(ClassyDataset):
             transform=transform,
             num_samples=num_samples,
             root=root,
-            download=download,
         )
