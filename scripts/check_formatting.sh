60490026ddbb27a10bfc1e30ea53c7fa66e6b51a
Mannat Singh <mannatsingh@fb.com>
2019-12-11 16:06:32 -0500

    Fix cd

--- scripts/check_formatting.sh
+++ scripts/check_formatting.sh
@@ -4,7 +4,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-if [ ! $(cd $(dirname "$0")/..) ]
+if [ ! "cd $(dirname $0)/.." ]
 then
     echo "Cannot cd to root dir"
     exit 1
