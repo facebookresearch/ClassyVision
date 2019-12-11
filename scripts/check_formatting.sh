c463c6cc25c38456febfcfe0a7de5875a8290065
Mannat Singh <mannatsingh@fb.com>
2019-12-11 16:55:03 -0500

    Add quotes for new lint warning

--- scripts/check_formatting.sh
+++ scripts/check_formatting.sh
@@ -4,7 +4,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-cd $(dirname $0)/.. || exit 1
+cd "$(dirname "$0")/.." || exit 1
 
 CMD="black"
 CHANGED_FILES="$(git diff --name-only master | grep '\.py$' | tr '\n' ' ')"
