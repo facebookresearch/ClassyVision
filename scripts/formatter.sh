7a73af4d6f87767c3365d2ce2b32de24ae2f2c86
Mannat Singh <mannatsingh@fb.com>
2019-12-16 19:48:34 +0530

    Mark classyvision and torchelastic as third party in isort

--- scripts/formatter.sh
+++ scripts/formatter.sh
@@ -4,14 +4,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-# NOTE: isort should be run in a Python enironment where all the packages
-# required by the package are installed and the project isn't in the
-# directory where isort is being called from. We fix the latter in the following
-# lines
-
-# cd to the scripts directory
-# this is needed for isort to work as expected
-cd "$(dirname "$0")/" || exit 1
+# cd to the project directory
+cd "$(dirname "$0")/.." || exit 1
 
 GIT_URL_1="https://github.com/facebookresearch/ClassyVision.git"
 GIT_URL_2="git@github.com:facebookresearch/ClassyVision.git"
@@ -33,9 +27,7 @@ fi
 # fetch upstream
 git fetch upstream
 
-CHANGED_FILES="$(git diff --name-only upstream/master | grep '\.py$')"
-# add ../ to all the files and remove newlines
-CHANGED_FILES="$(echo "$CHANGED_FILES" | sed 's/^/..\//' | tr '\n' ' ')"
+CHANGED_FILES="$(git diff --name-only upstream/master | grep '\.py$' | tr '\n' ' ')"
 
 if [ "$CHANGED_FILES" != "" ]
 then
@@ -51,7 +43,7 @@ then
     fi
 
     # run isort
-    cmd="isort $CHANGED_FILES"
+    cmd="isort --thirdparty classy_vision --thirdparty torchelastic $CHANGED_FILES"
     echo "Running command \"$cmd\""
     ($cmd)
 
@@ -68,6 +60,8 @@ CHANGED_FILES="$(git diff --name-only | grep '\.py$' | tr '\n' ' ')"
 
 if [ "$CHANGED_FILES" != "" ]
 then
+    echo "Running git diff:"
+    echo "$(git diff)"
     # need this so that CircleCI fails
     exit 1
 fi
