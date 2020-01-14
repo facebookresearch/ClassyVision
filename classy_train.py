facebookresearch/ClassyVision

commit b6bd886e2e6120a22710dc353dc85987737e4715

Fix issues in distributed launcher (#332)
Summary:
Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/332

Changes all default IP addresses to work with IPv6.

Reviewed By: aadcock, vreis

Differential Revision: D19254172

fbshipit-source-id: 9594e388e9b5c5798722639348a21281421829b0

classy_train.py
@@ -23,7 +23,7 @@ Example:
         $ python -m torch.distributed.launch \
                  --nnodes=1 \
                  --nproc_per_node=1 \
-                 --master_addr=127.0.0.1 \
+                 --master_addr=localhost \
                  --master_port=29500 \
                  --use_env \
                  classy_train.py \
@@ -88,6 +88,7 @@ def main(args, config):
     use_gpu = None
     if args.device is not None:
         use_gpu = args.device == "gpu"
+        assert torch.cuda.is_available(), "CUDA is unavailable"
 
     # LocalTrainer is used for a single node. DistributedTrainer will setup
     # training to use PyTorch's DistributedDataParallel.

classy_vision/generic/opts.py
@@ -8,7 +8,6 @@ import argparse
 import os
 
 import torch
-import torchvision
 from classy_vision.generic.util import is_pos_int
 
 

classy_vision/trainer/distributed_trainer.py
@@ -28,7 +28,7 @@ def _init_env_vars():
         os.environ["LOCAL_RANK"] = "0"
 
     if "MASTER_ADDR" not in os.environ or "MASTER_PORT" not in os.environ:
-        os.environ["MASTER_ADDR"] = "127.0.0.1"
+        os.environ["MASTER_ADDR"] = "localhost"
         os.environ["MASTER_PORT"] = "29500"
 
 

test/trainer_distributed_trainer_test.py
@@ -50,7 +50,7 @@ class TestDistributedTrainer(unittest.TestCase):
             cmd = f"""{sys.executable} -m torch.distributed.launch \
             --nnodes=1 \
             --nproc_per_node={num_processes} \
-            --master_addr=127.0.0.1 \
+            --master_addr=localhost \
             --master_port=29500 \
             --use_env \
             {self.path}/../classy_train.py \

commit 6860805446c861ad55f003fe5ffa7eab00efc787

Fix assertion in classy_train.py
Summary:
CUDA doesn't have to be available when training on CPUs.
Fix assertion introduced by https://github.com/facebookresearch/ClassyVision/pull/332

Differential Revision: D19392454

fbshipit-source-id: bd5c4e030df41716d87117df96e0f730e61be21c

classy_train.py
@@ -88,7 +88,7 @@ def main(args, config):
     use_gpu = None
     if args.device is not None:
         use_gpu = args.device == "gpu"
-        assert torch.cuda.is_available(), "CUDA is unavailable"
+        assert torch.cuda.is_available() or not use_gpu, "CUDA is unavailable"
 
     # LocalTrainer is used for a single node. DistributedTrainer will setup
     # training to use PyTorch's DistributedDataParallel.