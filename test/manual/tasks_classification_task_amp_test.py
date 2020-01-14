ffbf0e42b0c1ee8869ea3ffaf3af159bb253497b
Mannat Singh <mannatsingh@fb.com>
2020-01-14 07:53:29 -0800

    Mixed precision training (#338)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/338
    
    This adds a `set_amp_opt_level()` function to `ClassificationTask`, which can be used to train with `apex.amp`.
    
    Reviewed By: vreis
    
    Differential Revision: D18331200
    
    fbshipit-source-id: af9018bc2119b7ae0f1a2042d6f6d8d6c4fdb715

---
+++ test/manual/tasks_classification_task_amp_test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import copy
+import unittest
+from test.generic.config_utils import get_fast_test_task_config, get_test_task_config
+
+import torch
+from classy_vision.tasks import ClassificationTask, build_task
+from classy_vision.trainer import LocalTrainer
+
+
+class TestClassificationTaskAMP(unittest.TestCase):
+    def test_build_task(self):
+        config = get_test_task_config()
+        task = build_task(config)
+        self.assertTrue(isinstance(task, ClassificationTask))
+        # check that AMP is disabled by default
+        self.assertIsNone(task.amp_opt_level)
+
+        # test a valid AMP opt level
+        config = copy.deepcopy(config)
+        config["amp_opt_level"] = "O1"
+        task = build_task(config)
+        self.assertTrue(isinstance(task, ClassificationTask))
+
+        # test an invalid AMP opt level
+        config = copy.deepcopy(config)
+        config["amp_opt_level"] = "O5"
+        with self.assertRaises(Exception):
+            task = build_task(config)
+
+    @unittest.skipUnless(torch.cuda.is_available(), "This test needs a gpu to run")
+    def test_training(self):
+        config = get_fast_test_task_config()
+        config["amp_opt_level"] = "O2"
+        task = build_task(config)
+        trainer = LocalTrainer(use_gpu=True)
+        trainer.train(task)
