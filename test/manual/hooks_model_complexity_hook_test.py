9951a46f8150e789abeab4bf29ac293aab630db9
Mannat Singh <mannatsingh@fb.com>
2020-01-23 10:56:44 -0800

    Calculate activations to estimate model complexity (#357)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/357
    
    - Added a `compute_activations` function to `classy_vision.generic.profiler` which calculates the activations for a model. `ModelComplexityHook` prints out the activations as well.
    - Removed all the regex grepping in `hooks_model_complexity_hook_test` to check the computed values and instead added a separate test to do that.
    
    Differential Revision: D19526702
    
    fbshipit-source-id: 8e308e7d7153428e0c76c6b257a96d76e1e4e390

--- test/manual/hooks_model_complexity_hook_test.py
+++ test/manual/hooks_model_complexity_hook_test.py
@@ -4,7 +4,6 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import re
 import unittest
 from test.generic.config_utils import get_test_classy_task, get_test_model_configs
 
@@ -13,51 +12,21 @@ from classy_vision.models import build_model
 
 
 class TestModelComplexityHook(unittest.TestCase):
-    def test_model_complexity(self) -> None:
-        """
-        Test that the number of parameters and the FLOPs are calcuated correctly.
-        """
+    def test_model_complexity_hook(self) -> None:
         model_configs = get_test_model_configs()
-        expected_mega_flops = [4122, 4274, 106152]
-        expected_params = [25557032, 25028904, 43009448]
-        local_variables = {}
 
         task = get_test_classy_task()
         task.prepare()
 
+        local_variables = {}
+
         # create a model complexity hook
         model_complexity_hook = ModelComplexityHook()
 
-        for model_config, mega_flops, params in zip(
-            model_configs, expected_mega_flops, expected_params
-        ):
+        for model_config in model_configs:
             model = build_model(model_config)
 
             task.base_model = model
 
-            with self.assertLogs() as log_watcher:
+            with self.assertLogs():
                 model_complexity_hook.on_start(task, local_variables)
-
-            # there should be 2 log statements generated
-            self.assertEqual(len(log_watcher.output), 2)
-
-            # first statement - either the MFLOPs or a warning
-            if mega_flops is not None:
-                match = re.search(
-                    r"FLOPs for forward pass: (?P<mega_flops>[-+]?\d*\.\d+|\d+) MFLOPs",
-                    log_watcher.output[0],
-                )
-                self.assertIsNotNone(match)
-                self.assertEqual(mega_flops, float(match.group("mega_flops")))
-            else:
-                self.assertIn(
-                    "Model contains unsupported modules", log_watcher.output[0]
-                )
-
-            # second statement
-            match = re.search(
-                r"Number of parameters in model: (?P<params>[-+]?\d*\.\d+|\d+)",
-                log_watcher.output[1],
-            )
-            self.assertIsNotNone(match)
-            self.assertEqual(params, float(match.group("params")))
