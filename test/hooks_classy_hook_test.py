d4a93d521ea19a4a1148b729b30b20bb750aac5f
Aaron Adcock <aadcock@fb.com>
2020-03-18 11:33:38 -0700

    Add hook builder / registry (#401)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/401
    
    Pull Request resolved: https://github.com/fairinternal/ClassyVision/pull/61
    
    Add a hook builder / registry, similar to other classy abstractions.
    
    Differential Revision: D19755147
    
    fbshipit-source-id: eb9d848d9a353379c0a0834ec9d29aeec4ddb620

--- test/hooks_classy_hook_test.py
+++ test/hooks_classy_hook_test.py
@@ -4,11 +4,13 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import copy
 import unittest
 
-from classy_vision.hooks import ClassyHook
+from classy_vision.hooks import ClassyHook, build_hook, build_hooks, register_hook
 
 
+@register_hook("test_hook")
 class TestHook(ClassyHook):
     on_start = ClassyHook._noop
     on_phase_start = ClassyHook._noop
@@ -21,8 +23,26 @@ class TestHook(ClassyHook):
         self.state.a = a
         self.state.b = b
 
+    @classmethod
+    def from_config(cls, config):
+        return TestHook(config["a"], config["b"])
+
 
 class TestClassyHook(unittest.TestCase):
+    def test_hook_registry_and_builder(self):
+        config = {"name": "test_hook", "a": 1, "b": 2}
+        hook1 = build_hook(hook_config=config)
+        self.assertTrue(isinstance(hook1, TestHook))
+        self.assertTrue(hook1.state.a == 1)
+        self.assertTrue(hook1.state.b == 2)
+
+        hook_configs = [copy.deepcopy(config), copy.deepcopy(config)]
+        hooks = build_hooks(hook_configs=hook_configs)
+        for hook in hooks:
+            self.assertTrue(isinstance(hook, TestHook))
+            self.assertTrue(hook.state.a == 1)
+            self.assertTrue(hook.state.b == 2)
+
     def test_state_dict(self):
         a = 0
         b = {1: 2, 3: [4]}
