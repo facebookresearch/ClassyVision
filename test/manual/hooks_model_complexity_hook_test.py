d01e238ec150336c10eac225c03358b61f872dd4
Aaron Adcock <aadcock@fb.com>
2020-03-19 08:00:05 -0700

    Add from_config to ModelComplexityHook
    Summary: Add configuration to ModelComplexityHook
    
    Differential Revision: D20526995
    
    fbshipit-source-id: 063c1f2e392cce64cd81388c51236fa3a4dd9287

--- test/manual/hooks_model_complexity_hook_test.py
+++ test/manual/hooks_model_complexity_hook_test.py
@@ -7,11 +7,27 @@
 import unittest
 from test.generic.config_utils import get_test_classy_task, get_test_model_configs
 
-from classy_vision.hooks import ModelComplexityHook
+from classy_vision.hooks import ModelComplexityHook, build_hook
 from classy_vision.models import build_model
 
 
 class TestModelComplexityHook(unittest.TestCase):
+    def test_constructors(self) -> None:
+        """
+        Test that the hooks are constructed correctly.
+        """
+        config = {}
+
+        hook1 = ModelComplexityHook()
+        hook2 = ModelComplexityHook.from_config(config)
+        config["name"] = "model_complexity"
+        hook3 = build_hook(config)
+        del config["name"]
+
+        self.assertTrue(isinstance(hook1, ModelComplexityHook))
+        self.assertTrue(isinstance(hook2, ModelComplexityHook))
+        self.assertTrue(isinstance(hook3, ModelComplexityHook))
+
     def test_model_complexity_hook(self) -> None:
         model_configs = get_test_model_configs()
 
