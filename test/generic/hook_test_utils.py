402dfbf20a0f3be059fdcab14903dc110ca14f0a
Aaron Adcock <aadcock@fb.com>
2020-03-19 19:56:44 -0700

    Add test utility for hooks
    Summary: Added a base test class for the hook constructor tests which have a lot of copy pasta
    
    Differential Revision: D20543898
    
    fbshipit-source-id: a0eb00ae3f1311ef571410dcb44aae2b961386e1

---
+++ test/generic/hook_test_utils.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import unittest
+
+
+class HookTestBase(unittest.TestCase):
+
+    def constructor_test_helper(
+            self,
+            arg_list,
+            config,
+            hook_type,
+            hook_registry_name=None,
+            invalid_configs=None,
+    ):
+        hook1 = hook_type(*arg_list)
+        self.assertTrue(isinstance(hook1, hook_type))
+
+        hook2 = hook_type.from_config(config)
+        self.assertTrue(isinstance(hook2, hook_type))
+
+        if hook_registry_name is not None:
+            config["name"] = hook_registry_name
+            hook3 = build_hook(config)
+            del config["name"]
+            self.assertTrue(isinstance(hook3, hook_type))
+
+        if invalid_configs is not None:
+            # Verify assert logic works correctly
+            for cfg in invalid_configs:
+                with self.assertRaises(AssertionError):
+                    hook_type.from_config(cfg)
