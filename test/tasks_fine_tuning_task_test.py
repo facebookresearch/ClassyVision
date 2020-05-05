f7438691065b2a9305bbce0bbddbb5a58fa11ccf
Mannat Singh <mannatsingh@fb.com>
2020-05-04 20:35:31 -0700

    Allow specifying checkpoint path in tasks (#503)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/503
    
    - Changed the `set_checkpoint` and `set_pretrained_checkpoint` calls to take the `checkpoint_path`.
      - The tests still pass the checkpoint, created `_set_checkpoint_dict` for the old functionality.
    - Added test cases and fixed tests
      - The fine tuning tests were too slow and were timing out, made them faster
    
    Reviewed By: vreis
    
    Differential Revision: D21231849
    
    fbshipit-source-id: 263da6cebbd3c489656fc1dd478a2545b336420a

--- test/tasks_fine_tuning_task_test.py
+++ test/tasks_fine_tuning_task_test.py
@@ -20,20 +20,20 @@ class TestFineTuningTask(unittest.TestCase):
         return compare_model_state(self, state_1, state_2, check_heads=check_heads)
 
     def _get_fine_tuning_config(
-        self, head_num_classes=1000, pretrained_checkpoint=False
+        self, head_num_classes=100, pretrained_checkpoint=False
     ):
         config = get_fast_test_task_config(head_num_classes=head_num_classes)
         config["name"] = "fine_tuning"
-        config["num_epochs"] = 10
+        config["num_epochs"] = 2
 
         if pretrained_checkpoint:
             config["pretrained_checkpoint"] = "/path/to/pretrained/checkpoint"
 
         return config
 
-    def _get_pre_train_config(self, head_num_classes=1000):
+    def _get_pre_train_config(self, head_num_classes=100):
         config = get_fast_test_task_config(head_num_classes=head_num_classes)
-        config["num_epochs"] = 10
+        config["num_epochs"] = 2
         return config
 
     def test_build_task(self):
@@ -62,7 +62,7 @@ class TestFineTuningTask(unittest.TestCase):
             fine_tuning_task.prepare()
 
         # test: prepare should succeed after pre-trained checkpoint is set
-        fine_tuning_task.set_pretrained_checkpoint(checkpoint)
+        fine_tuning_task._set_pretrained_checkpoint_dict(checkpoint)
         fine_tuning_task.prepare()
 
         # test: prepare should succeed if a pre-trained checkpoint is provided in the
@@ -73,21 +73,20 @@ class TestFineTuningTask(unittest.TestCase):
             return_value=checkpoint,
         ):
             fine_tuning_task = build_task(fine_tuning_config)
-
         fine_tuning_task.prepare()
 
         # test: a fine tuning task with incompatible heads with a manually set
         # pre-trained checkpoint should fail to prepare if the heads are not reset
         fine_tuning_config = self._get_fine_tuning_config(head_num_classes=10)
         fine_tuning_task = build_task(fine_tuning_config)
-        fine_tuning_task.set_pretrained_checkpoint(checkpoint)
+        fine_tuning_task._set_pretrained_checkpoint_dict(checkpoint)
 
         with self.assertRaises(Exception):
             fine_tuning_task.prepare()
 
         # test: a fine tuning task with incompatible heads with a manually set
         # pre-trained checkpoint should succeed to prepare if the heads are reset
-        fine_tuning_task.set_pretrained_checkpoint(
+        fine_tuning_task._set_pretrained_checkpoint_dict(
             copy.deepcopy(checkpoint)
         ).set_reset_heads(True)
 
@@ -104,7 +103,6 @@ class TestFineTuningTask(unittest.TestCase):
             return_value=copy.deepcopy(checkpoint),
         ):
             fine_tuning_task = build_task(fine_tuning_config)
-
         with self.assertRaises(Exception):
             fine_tuning_task.prepare()
 
@@ -115,20 +113,20 @@ class TestFineTuningTask(unittest.TestCase):
         fine_tuning_task.prepare()
 
     def test_train(self):
-        pre_train_config = self._get_pre_train_config(head_num_classes=1000)
+        pre_train_config = self._get_pre_train_config(head_num_classes=100)
         pre_train_task = build_task(pre_train_config)
         trainer = LocalTrainer()
         trainer.train(pre_train_task)
         checkpoint = get_checkpoint_dict(pre_train_task, {})
 
-        for reset_heads, heads_num_classes in [(False, 1000), (True, 200)]:
+        for reset_heads, heads_num_classes in [(False, 100), (True, 20)]:
             for freeze_trunk in [True, False]:
                 fine_tuning_config = self._get_fine_tuning_config(
                     head_num_classes=heads_num_classes
                 )
                 fine_tuning_task = build_task(fine_tuning_config)
                 fine_tuning_task = (
-                    fine_tuning_task.set_pretrained_checkpoint(
+                    fine_tuning_task._set_pretrained_checkpoint_dict(
                         copy.deepcopy(checkpoint)
                     )
                     .set_reset_heads(reset_heads)
