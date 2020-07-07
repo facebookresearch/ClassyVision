7d92b126afa00be43b2293b1ad7c2abebfa2be74
Benjamin Lefaudeux <lefaudeux@fb.com>
2020-07-06 22:11:14 -0700

    Fix a difference in the SE layer squeeze (#559)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/559
    
    The reduction ratio was not applied in the same way in between the original RegNet implementation and the one in ClassyVision. Fixing this
    
    Reviewed By: mannatsingh
    
    Differential Revision: D22400429
    
    fbshipit-source-id: bce9a9b392aafbb0a7b978096f5ed51355fd936e

--- classy_vision/models/squeeze_and_excitation_layer.py
+++ classy_vision/models/squeeze_and_excitation_layer.py
@@ -5,16 +5,30 @@
 # LICENSE file in the root directory of this source tree.
 
 
+from typing import Optional
+
 import torch.nn as nn
 
 
 class SqueezeAndExcitationLayer(nn.Module):
     """Squeeze and excitation layer, as per https://arxiv.org/pdf/1709.01507.pdf"""
 
-    def __init__(self, in_planes, reduction_ratio=16):
+    def __init__(
+        self,
+        in_planes,
+        reduction_ratio: Optional[int] = 16,
+        reduced_planes: Optional[int] = None,
+    ):
         super().__init__()
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        reduced_planes = in_planes // reduction_ratio
+
+        # Either reduction_ratio is defined, or out_planes is defined,
+        # neither both nor none of them
+        assert bool(reduction_ratio) != bool(reduced_planes)
+
+        reduced_planes = (
+            in_planes // reduction_ratio if reduced_planes is None else reduced_planes
+        )
         self.excitation = nn.Sequential(
             nn.Conv2d(in_planes, reduced_planes, kernel_size=1, stride=1, bias=True),
             nn.ReLU(),
