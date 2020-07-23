fe4a1f89e0651bd0216229a6cb22bf14474c9a85
Benjamin Lefaudeux <lefaudeux@fb.com>
2020-07-23 09:33:38 -0700

    Choose the source when broadcasting (#573)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/573
    
    Second take, make it possible to choose the broadcast source to better match the point-to-point needs
    
    Reviewed By: mannatsingh
    
    Differential Revision: D22650026
    
    fbshipit-source-id: cca87dd73e5b14813f9bfa3e31a539a7c42c2d81

--- test/generic_distributed_util_test.py
+++ test/generic_distributed_util_test.py
@@ -6,6 +6,7 @@
 
 import tempfile
 import unittest
+from typing import Any, Dict, List
 
 import torch
 from classy_vision.generic.distributed_util import broadcast_object
@@ -18,14 +19,14 @@ def init_and_run_process(
     torch.distributed.init_process_group(
         backend, init_method=f"file://{filename}", rank=rank, world_size=world_size
     )
-    r = fn(input)
+    r = fn(*input)
     q.put(r)
 
     wait_event.wait()
     return
 
 
-def run_in_process_group(world_size, filename, fn, inputs):
+def run_in_process_group(filename: str, calls: List[Dict[str, Any]]):
     if torch.distributed.is_initialized():
         torch.distributed.destroy_process_group()
     processes = []
@@ -34,10 +35,18 @@ def run_in_process_group(world_size, filename, fn, inputs):
 
     # run the remaining processes
     # for rank in range(world_size - 1):
-    for rank in range(world_size):
+    for rank, call in enumerate(calls):
         p = Process(
             target=init_and_run_process,
-            args=(rank, world_size, filename, fn, inputs[rank], q, wait_event),
+            args=(
+                rank,
+                call["world_size"],
+                filename,
+                call["function"],
+                call["inputs"],
+                q,
+                wait_event,
+            ),
         )
         p.start()
         processes.append(p)
@@ -57,20 +66,59 @@ def run_in_process_group(world_size, filename, fn, inputs):
 
 
 class TestDistributedUtil(unittest.TestCase):
-    def test_broadcast_object(self):
-        world_size = 3
-        for obj in [
+    @staticmethod
+    def _get_test_objects():
+        return [
             {"a": 12, "b": [2, 3, 4], "tensor": torch.randn(10, 10)},
             None,
             {"tensor": torch.randn(10000, 10000)},  # 400 MB
-        ]:
+        ]
+
+    def test_broadcast_object(self):
+        world_size = 3
+        for obj in self._get_test_objects():
             filename = tempfile.NamedTemporaryFile(delete=True).name
             inputs = [None] * world_size
-            # only the master worker has the object
-            inputs[0] = obj
-            results = run_in_process_group(
-                world_size, filename, broadcast_object, inputs
-            )
+            inputs[0] = obj  # only the master worker has the object
+
+            calls = [
+                {"world_size": world_size, "function": broadcast_object, "inputs": [i]}
+                for i in inputs
+            ]
+            results = run_in_process_group(filename, calls)
+
+            # check that all replicas got identical objects
+            self.assertEqual(len(results), world_size)
+
+            for result in results:
+                if isinstance(obj, dict):
+                    for key in obj:
+                        if key == "tensor":
+                            self.assertTrue(torch.allclose(result[key], obj[key]))
+                        else:
+                            self.assertEqual(result[key], obj[key])
+                else:
+                    self.assertEqual(result, obj)
+
+    def test_broadcast_object_pick_source(self):
+        world_size = 3
+        for obj in self._get_test_objects():
+            filename = tempfile.NamedTemporaryFile(delete=True).name
+            inputs = [None] * world_size
+            source_rank = 1
+            inputs[source_rank] = obj  # only the rank 1 worker has the object
+
+            calls = [
+                {
+                    "world_size": world_size,
+                    "function": broadcast_object,
+                    "inputs": [i, source_rank],
+                }
+                for i in inputs
+            ]
+            results = run_in_process_group(filename, calls)
+
+            # check that all replicas got identical objects
             self.assertEqual(len(results), world_size)
             for result in results:
                 if isinstance(obj, dict):
