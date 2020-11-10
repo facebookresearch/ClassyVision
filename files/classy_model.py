624bbd0bfd92f7cbc08d8d95bcdb15f622ff4836
Mannat Singh <mannatsingh@fb.com>
2020-11-10 02:07:25 -0500

    Update latest version of site

---
+++ files/classy_model.py
@@ -0,0 +1,273 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Classy Models
+# 
+# Before reading this, please go over the [Getting Started tutorial](https://classyvision.ai/tutorials/getting_started).
+# 
+# Working with Classy Vision requires models to be instances of `ClassyModel`. A `ClassyModel` is an instance of `torch.nn.Module`, but packed with a lot of extra features! 
+# 
+# If your model isn't implemented as a `ClassyModel`, don't fret - you can easily convert it to one in one line.
+# 
+# In this tutorial, we will cover:
+# 1. Using Classy Models
+# 1. Getting and setting the state of a model
+# 1. Heads: Introduction & Using Classy Heads
+# 1. Creating a custom Classy Model
+# 1. Converting any PyTorch model to a Classy Model
+
+# ## Using Classy Models
+# As `ClassyModel`s are also instances of `nn.Module`, they can be treated as any normal PyTorch model.
+
+# In[1]:
+
+
+import torch
+from classy_vision.models import build_model
+
+
+model = build_model({"name": "resnet50"})
+input = torch.ones(10, 3, 224, 224)  # a batch of 10 images with 3 channels with dimensions of 224 x 224
+output = model(input)
+
+
+# ## Getting and setting the state of a model
+# 
+# Classy Vision provides the functions `get_classy_state()` and `set_classy_state()` to fetch and save the state of the models. These are considered drop-in replacements for the [`torch.nn.Module.state_dict`](https://pytorch.org/docs/stable/nn.html#torch.nn.Module.state_dict) and [`torch.nn.Module.load_state_dict()`](https://pytorch.org/docs/stable/nn.html#torch.nn.Module.load_state_dict) functions and work similarly. For more information, refer to the [docs](https://classyvision.ai/api/models.html#classy_vision.models.ClassyModel).
+
+# In[2]:
+
+
+state = model.get_classy_state()
+
+model.set_classy_state(state)
+
+
+# ## Heads: Introduction & Using Classy Heads
+# 
+# A lot of work in Computer Vision utilizes the concept of re-using a trunk model, like a ResNet 50, and using it for various tasks. This is accomplished by attaching different "heads" to the end of the trunk. 
+# 
+# Some use cases involve re-training a model trained with a certain head by removing the old head and attaching a new one. This is a special case of fine tuning. If you are interested in fine tuning your models, there's a [tutorial for that as well](https://classyvision.ai/tutorials/fine_tuning)! But first, let's understand the basics.
+# 
+# Normally, attaching heads or changing them requires users to write code and update their model implementations. Classy Vision does all of this work for you - all of this happens under the hood, with no work required by users!
+# 
+# All you need to do is decide which `ClassyHead` you want to attach to your model and where. We will use a simple fully connected head in our example, and attach it to the output of the `block3-2` module of our model. Note that a head can be attached to any module, as long as the name of the module is unique.
+
+# In[3]:
+
+
+from classy_vision.heads import FullyConnectedHead
+
+
+# a resnet 50 model's trunk outputs a tensor of 2048 dimension, which will be the
+# in_plane of out head
+#
+# let's say we want a 100 dimensional output
+#
+# Tip: you can use build_head() as well to create a head instead of initializing the
+# class directly
+head = FullyConnectedHead(unique_id="default", num_classes=100, in_plane=2048)
+
+# let's attach this head to our model
+model.set_heads({"block3-2": [head]})
+
+output = model(input)
+assert output.shape == (10, 100)
+
+# let's change the head one more time
+head = FullyConnectedHead(unique_id="default", num_classes=10, in_plane=2048)
+
+model.set_heads({"block3-2": [head]})
+
+output = model(input)
+assert output.shape == (10, 10)
+
+
+# Classy Vision supports attaching multiple heads to one or more blocks as well, but that is an advanced concept which this tutorial does not cover. For inquisitive users, here is an example -
+
+# In[4]:
+
+
+head_1_1 = FullyConnectedHead(unique_id="1_1", num_classes=10, in_plane=1024)
+head_1_2 = FullyConnectedHead(unique_id="1_2", num_classes=20, in_plane=1024)
+head_2 = FullyConnectedHead(unique_id="2", num_classes=100, in_plane=2048)
+
+# we can attach these heads to our model at different blocks
+model.set_heads({"block2-2": [head_1_1, head_1_2], "block3-2": [head_2]})
+
+output = model(input)
+
+
+# ## Creating a custom Classy Model
+# 
+# This section will demonstrate: (1) how to create a custom model within Classy Vision; (2) how to integrate your model with Classy Vision's configuration system; (3) how to use the model for training and inference;
+# 
+# ### 1. Defining a model
+# 
+# Creating a new model in Classy Vision is as simple as creating one within PyTorch. The model needs to derive from `ClassyModel` and implement a `forward` method to perform inference. `ClassyModel` inherits from [`torch.nn.Module`](https://pytorch.org/docs/stable/nn.html#module), so it works exactly as you would expect.
+
+# In[5]:
+
+
+import torch.nn as nn
+
+from classy_vision.models import ClassyModel
+
+
+class MyModel(ClassyModel):
+    def __init__(self, num_classes):
+        super().__init__()
+        
+        # Average all the pixels, generate one output per class
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        num_channels = 3
+        self.fc = nn.Linear(num_channels, num_classes)
+        
+    def forward(self, x):
+        # perform average pooling
+        out = self.avgpool(x)
+
+        # reshape the output and apply the fc layer
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        return out
+
+
+# Now we can start using this model for training. Take a look at our [Getting started](https://classyvision.ai/tutorials/getting_started) tutorial for more details on how to train a model from a Jupyter notebook.
+
+# In[6]:
+
+
+from classy_vision.tasks import ClassificationTask
+
+my_model = MyModel(num_classes=1000)
+my_task = ClassificationTask().set_model(my_model)
+
+
+# ### 2. Integrating it with the configuration system
+# 
+# Classy Vision is also able to read a configuration file and instantiate the model. This is useful to keep your experiments organized and reproducible. For that, you have to:
+# 
+# - Implement a `from_config` method
+# - Add the `register_model` decorator to `MyModel`
+
+# In[7]:
+
+
+import torch.nn as nn
+
+from classy_vision.models import ClassyModel, register_model
+
+
+@register_model("my_model")
+class MyModel(ClassyModel):
+    def __init__(self, num_classes):
+        super().__init__()
+        
+        # Average all the pixels, generate one output per class
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        num_channels = 3
+        self.fc = nn.Linear(num_channels, num_classes)
+
+    @classmethod
+    def from_config(cls, config):
+        # This method takes a configuration dictionary 
+        # and returns an instance of the class. In this case, 
+        # we'll let the number of classes be configurable.
+        return cls(num_classes=config["num_classes"])
+        
+    def forward(self, x):
+        # perform average pooling
+        out = self.avgpool(x)
+
+        # reshape the output and apply the fc layer
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        return out
+
+
+# Now we can start using this model in our configurations. The argument passed to `register_model` is used to identify the model class in the configuration:
+
+# In[8]:
+
+
+from classy_vision.models import build_model
+import torch
+
+model_config = {
+    "name": "my_model",
+    "num_classes": 3
+}
+my_model = build_model(model_config)
+assert isinstance(my_model, MyModel)
+
+# my_model inherits from torch.nn.Module, so inference works as usual:
+x = torch.rand((1, 3, 200, 200))
+with torch.no_grad():
+    print(my_model(x))
+
+
+# Now that your model is integrated with the configuration system, you can train it using `classy_train.py` as described in the [Getting started](https://classyvision.ai/tutorials/getting_started) tutorial, no further changes are needed! Just make sure the code defining your model is in the `models` folder of your classy project.
+
+# ## Converting any PyTorch model to a Classy Model
+# 
+# Any model can be converted to a Classy Model with a simple function call - `ClassyModel.from_model()`
+
+# In[9]:
+
+
+from torchvision.models import resnet18
+from classy_vision.models import ClassyModel
+
+
+model = resnet18()
+classy_model = ClassyModel.from_model(model)
+output = classy_model(input)
+assert output.shape == (10, 1000)
+
+
+# In fact, as soon as a model becomes a Classy Model, it gains all its abilities as well, including the ability to attach heads! Let us inspect the original model to see the modules it comprises.
+
+# In[10]:
+
+
+model
+
+
+# It seems that the final trunk layer of this model is called `layer4`. Let's try to attach heads here.
+
+# In[11]:
+
+
+# the output of layer4 is 512 dimensional
+head = FullyConnectedHead(unique_id="default", num_classes=10, in_plane=512)
+
+classy_model.set_heads({"layer4": [head]})
+
+output = classy_model(input)
+assert output.shape == (10, 10)  # it works!
+
+
+# You might be wondering how to figure out the `in_plane` for any module. A simple trick is to try attaching any head and noticing the `Exception` if there is a size mismatch!
+
+# In[12]:
+
+
+try:
+    head = FullyConnectedHead(unique_id="default", num_classes=10, in_plane=1234)
+
+    classy_model.set_heads({"layer4": [head]})
+
+    output = classy_model(input)
+
+except Exception as e:
+    print(e)
+
+
+# The error tells us that the size should be 512.
+
+# ## Conclusion
+# 
+# In this tutorial, we covered how to use Classy Models, how to get and set their state, and how to create our own models & integrating them with the configuration system. We also got familiarized with the concept of heads and how they work with Classy Vision. Lastly, we learned how we can easily convert any PyTorch models to Classy Models and unlock all the features they provide.
+# 
+# For more information, refer to our [API docs](https://classyvision.ai/api/).
---
+++ files/ray_aws.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Distributed training on AWS
+
+# In this tutorial we will learn: 
+# 1. How to start a cluster on AWS for use with Classy Vision; 
+# 2. How to start a new project on the cluster; 
+# 3. How to launch training jobs on the cluster;
+# 
+# ## 1. Setup
+# 
+# Make sure you have Classy Vision installed, as described in our [Getting started](https://classyvision.ai/tutorials/getting_started) tutorial. 
+# 
+# For this tutorial we will also need the Classy Vision sources, you can clone it with this command (on your terminal):
+
+# In[ ]:
+
+
+get_ipython().system(' git clone https://github.com/facebookresearch/ClassyVision.git')
+
+
+# In this tutorial we'll use [Ray](https://github.com/ray-project/ray) to manage the AWS resources. Install Ray and all its required dependencies with:
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('cd', './ClassyVision/examples/ray')
+get_ipython().system(' pip install -r requirements.txt')
+
+
+# You should also set up your AWS CLI and credentials as described [here](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html#cli-quick-configuration). To make sure everything is working, run on your terminal:
+
+# In[ ]:
+
+
+get_ipython().system(' aws ec2 describe-instances')
+
+
+# That should print a JSON file with all your current AWS instances (or empty if you don't have any). 
+
+# ## 2. Cluster setup
+
+# We have a sample cluster configuration file stored in the Classy Vision repository, under `./examples/ray/cluster_config.yml`. Let's verify that Ray can start the cluster appropriately:
+
+# In[ ]:
+
+
+get_ipython().system(' ray up cluster_config.yml -y')
+
+
+# That will take about 10 minutes, and at the end you should see a message explaining how to connect to the cluster. Assuming everything worked successfully, now tear down the cluster:
+
+# In[ ]:
+
+
+get_ipython().system(' ray down cluster_config.yml -y')
+
+
+# We will now set up an EFS volume to store our code and datasets. Follow [this tutorial](https://aws.amazon.com/getting-started/tutorials/create-network-file-system/) to setup the EFS volume in your AWS account. 
+# 
+# When you're done with that tutorial, go back to the EFS section in the AWS console, find your filesystem there and click `Manage file system access`. Add the `ray-autoscaler-default` security group to the list of security groups allowed to use your EFS volume. That security group should have been created by the `ray up` command we ran earlier.
+# 
+# You should now have an identifier for your EFS volume. Open `cluster_config.yml` in your favorite text editor and replace `{{FileSystemId}}` with your own EFS id. We are now ready to launch our cluster again:
+
+# In[ ]:
+
+
+get_ipython().system(' ray up cluster_config.yml')
+
+
+# ## 3. Create a project
+# 
+# When it's done, let's attach to the head node of the cluster:
+
+# In[ ]:
+
+
+get_ipython().system(' ray attach cluster_config.yml')
+
+
+# That will give you an SSH session into the head node, which coordinates all the worker nodes in Ray. In our example configuration file, the head node is a CPU-only machine, and the workers all have GPUs.
+# 
+# Both the head node and the worker nodes will have the same EFS volume mounted, so we'll use that to send code from the head to the workers. The following commands are meant to run on the head node (e.g. in the terminal prompt you got from `ray attach`). Let's start a project in the EFS folder:
+
+# In[ ]:
+
+
+$ cd efs
+$ classy-project my_project
+
+
+# ## 4. Start training
+# 
+# Classy Vision comes with a launcher analogous to `torch.distributed.launch`, but that launches jobs on multiple machines using Ray. To use it, simply run:
+
+# In[ ]:
+
+
+$ python -m classy_vision.distributed.launch_ray --nnodes=2 --use_env ~/efs/my_project/classy_train.py --config ~/efs/my_project/configs/template_config.json --distributed_backend ddp
+
+
+# Your first time running this you might see logs like `Not enough GPUs available`. That's normal, and it's because the worker nodes are still being set up. The `ray up` command should have printed a command line you can use to follow their progress. But there's no need to do anything, the launcher will wait until the workers are available and execute the command automatically.
+# 
+# That's it! When that command is done it should print the folder where the checkpoints are.
+# 
+# > Note that we specified the full absolute path for the config in the argument list. That's because the `classy_train.py` command is running on a remote machine and we are relying on the fact that the EFS folder is mounted at exactly the same location on the head and worker nodes. Keep that in mind if you modify this setup.
+# 
+# > Remember to tear down the cluster with `ray down cluster_config.yml` when you're done. You will be billed as long as the machines are up, even when not using them.
+
+# ## 5. Troubleshooting
+# 
+# If you hit an error during this tutorial, here are a few things that might help to debug what is going on:
+# 
+# ### Make sure all workers have initialized properly
+# 
+# When the `ray up` command finishes, it prints a command line to tail the logs. It should look like:
+# ```bash
+# ray exec cluster_config.yml 'tail -n 100 -f /tmp/ray/session_*/logs/monitor*'
+# ```
+# 
+# Run that command and look for any errors. When the workers are done initializing, you should see `-- StandardAutoscaler: 2/2 target nodes (0 pending)` printed repeatedly on the logs.
+# 
+# ### Make sure EFS volumes are mounted on all machines
+# 
+# Sometimes the EFS package fails to install on workers. To verify EFS is working, get the worker node IPs with `ray get-worker-ips cluster_config.yml`, then ssh on them with:
+# ```bash
+# ssh -i ~/.ssh/ray-autoscaler_us-west-2.pem ubuntu@<WORKER-IP>
+# ```
+# 
+# Once in a worker machine, run `df -h` to list all the current mounts. Verify `/home/ubuntu/efs` is on that list. If it's not, look for the EFS setup commands on the `cluster_config.yml` file and run them yourself. That should clarify what the issue is. If you didn't setup the EFS security groups correctly (as described in step 2), the `mount` command will hang for a few minutes then fail.
+# 
+# ## 6. Conclusion
+# 
+# In this tutorial we covered how to start using Classy Vision on AWS using Ray. For more information about Ray, check out their [repository](https://github.com/ray-project/ray). The next tutorials ([[1]](https://classyvision.ai/tutorials/classy_model), [[2]](https://classyvision.ai/tutorials/classy_loss), [[3]](https://classyvision.ai/tutorials/classy_dataset)) will demonstrate how to customize the project created by the `classy-project` utility for your own needs. To learn more about how to train models in Classy Vision and how to use Tensorboard to visualize training progress, check out our [Getting started](https://classyvision.ai/tutorials/getting_started) tutorial.
