7c58f6dc29f92dfb22f8dc3970e445faec95d147
Mannat Singh <mannatsingh@fb.com>
2020-11-09 21:28:40 -0800

    Open source vision transformers (#646)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/646
    
    Open source Vision Transformers from https://arxiv.org/abs/2010.11929
    
    Reviewed By: vreis
    
    Differential Revision: D24840754
    
    fbshipit-source-id: b5bbe1fd77aca2730c36edd472fa52d2bedf4b61

---
+++ classy_vision/models/lecun_normal_init.py
@@ -0,0 +1,11 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import torch.nn as nn
+import math
+
+
+def lecun_normal_init(tensor, fan_in):
+    nn.init.trunc_normal_(tensor, std=math.sqrt(1 / fan_in))
