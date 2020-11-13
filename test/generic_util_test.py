72deb06823ae2d28ee1b79734020766045427ddc
Albert Gordo <agordo@fb.com>
2020-11-13 13:00:18 -0800

    Add support for more generic inputs to "Recursive copy to" (#653)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/653
    
    Add support to other types to recursive_copy_to_device.
    In particular, as long as the input has a "to" method, recursive_copy_to_device will be able to handle it. This opens support to more complex inputs, e.g. classes.
    
    Reviewed By: mannatsingh
    
    Differential Revision: D24910581
    
    fbshipit-source-id: f3945630b722740d9f0cc47d69a08972640f0b85

--- test/generic_util_test.py
+++ test/generic_util_test.py
@@ -46,9 +46,23 @@ def get_mock_tensor(mock_class):
 
 
 class TestUtilMethods(unittest.TestCase):
+    class StructuredInput:
+        def __init__(self, tensor_a, tensor_b):
+            self.tensor_a = tensor_a
+            self.tensor_b = tensor_b
+
+        def to(self, device, non_blocking):
+            return TestUtilMethods.StructuredInput(
+                tensor_a=self.tensor_a.to(device=device, non_blocking=non_blocking),
+                tensor_b=self.tensor_b.to(device=device, non_blocking=non_blocking),
+            )
+
     def test_recursive_copy_to_gpu(self):
         tensor_a = get_mock_tensor()
         tensor_b = get_mock_tensor()
+        tensor_structured = TestUtilMethods.StructuredInput(
+            tensor_a=get_mock_tensor(), tensor_b=get_mock_tensor()
+        )
 
         valid_gpu_copy_value = tensor_a
         gpu_value = util.recursive_copy_to_gpu(valid_gpu_copy_value)
@@ -75,6 +89,11 @@ class TestUtilMethods(unittest.TestCase):
 
         value = {"a": "b"}
         self.assertEqual(value, util.recursive_copy_to_gpu(value))
+
+        valid_gpu_copy_structured = tensor_structured
+        gpu_value = util.recursive_copy_to_gpu(valid_gpu_copy_structured)
+        self.assertTrue(gpu_value.tensor_a.is_cuda)
+        self.assertTrue(gpu_value.tensor_b.is_cuda)
 
     _json_config_file = ROOT / "generic_util_json_blob_test.json"
 
