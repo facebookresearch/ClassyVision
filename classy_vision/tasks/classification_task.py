482d7de8fdcd50ae283e3242f7e1eb99b59c0e15
Abhishek Kadian <akadian@fb.com>
2020-11-13 10:26:32 -0800

    Optimizer inf/nan loss, remove unused optimizer backward
    Summary:
    * Check for loss sanity before calling backward and step, D24740329 (https://github.com/facebookresearch/ClassyVision/commit/1ba6b0354e87c41b5eb26934888d66a55e198b37) modified the order of check.
    * Remove unused optimizer backward, previously this was re-introduced in D21080585 (https://github.com/facebookresearch/ClassyVision/commit/4a627a1e97d72d02eeeefac52d3d71b13822e835) and rendered useless in D24740329 (https://github.com/facebookresearch/ClassyVision/commit/1ba6b0354e87c41b5eb26934888d66a55e198b37).
    
    Differential Revision: D24955125
    
    fbshipit-source-id: 993485cb7091ae4d678d69ec72b662331e663009

--- classy_vision/tasks/classification_task.py
+++ classy_vision/tasks/classification_task.py
@@ -27,10 +27,10 @@ from classy_vision.generic.util import (
     Timer,
     copy_model_to_gpu,
     load_and_broadcast_checkpoint,
+    master_params,
     recursive_copy_to_gpu,
     split_batchnorm_params,
     update_classy_state,
-    master_params,
 )
 from classy_vision.hooks import CheckpointHook, ClassyHook, build_hooks
 from classy_vision.losses import ClassyLoss, build_loss
@@ -985,6 +985,8 @@ class ClassificationTask(ClassyTask):
         if (update_idx % self.optimizer_period) == 0:
             self.optimizer.zero_grad()
 
+        self.check_inf_nan(loss)
+
         if self.amp_args is not None:
             with apex.amp.scale_loss(loss, self.optimizer.optimizer) as scaled_loss:
                 scaled_loss.backward()
@@ -1000,8 +1002,6 @@ class ClassificationTask(ClassyTask):
                 self._clip_gradients(self.clip_grad_norm)
 
             self.optimizer.step(where=self.where)
-
-        self.check_inf_nan(loss)
 
     def _rescale_gradients(self, scale):
         for param in master_params(self.optimizer):
