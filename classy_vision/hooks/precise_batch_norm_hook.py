f9e1a2f3bf5c9e803dbd01c02ba9b445d62ccdd1
Zhicheng Yan <zyan3@fb.com>
2020-12-09 11:43:07 -0800

    reduce memory use of PreciseBatchNormHook (#605)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/605
    
    In current `PreciseBatchNormHook`, it caches samples which are used to compute precise BN at the end of phase. The memory use will increase significantly.
    
    For example, in video classification, when cache size is 10,000, input frame is 16, spatial resolution is 224, and no. of gpus is 8, the total cache size is (10000 * 16 * 3* 224 * 224 * 8) = 192 GB on a single host, which is hard to afford.
    
    Thus, improve the implementation to by adding an argument `cache_sample`.  If false, we do not cache samples, but re-create data loader at the end of training phase.
    
    Reviewed By: mannatsingh
    
    Differential Revision: D23191690
    
    fbshipit-source-id: 3874ef3afcd71b7124a8b047c4be55d6eaab634f

--- classy_vision/hooks/precise_batch_norm_hook.py
+++ classy_vision/hooks/precise_batch_norm_hook.py
@@ -4,6 +4,9 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import logging
+import math
+
 import torch
 from classy_vision.generic.util import (
     get_batchsize_per_replica,
@@ -14,11 +17,12 @@ from classy_vision.hooks import ClassyHook, register_hook
 from fvcore.nn.precise_bn import update_bn_stats
 
 
-def _get_iterator(cache, use_gpu):
-    for elem in cache:
+def _get_iterator(data_iter, use_gpu):
+    for elem in data_iter:
         if use_gpu:
             elem = recursive_copy_to_gpu(elem, non_blocking=True)
-        yield elem
+
+        yield elem["input"]
 
 
 @register_hook("precise_bn")
@@ -32,48 +36,83 @@ class PreciseBatchNormHook(ClassyHook):
     fvcore/nn/precise_bn.py>`_ for more information.
     """
 
-    on_start = ClassyHook._noop
-    on_phase_start = ClassyHook._noop
-    on_step = ClassyHook._noop
     on_end = ClassyHook._noop
 
-    def __init__(self, num_samples: int) -> None:
+    def __init__(self, num_samples: int, cache_samples: bool = False) -> None:
         """The constructor method of PreciseBatchNormHook.
 
         Caches the required number of samples on the CPU during train phases
 
         Args:
             num_samples: Number of samples to calculate the batch norm stats per replica
+            cache_samples: If True, we cache samples at training stage. This avoids re-creating
+                data loaders, but consumes more memory. If False, we re-create data loader at the
+                end of phase, which might be slow for large dataset, but saves memory.
         """
         super().__init__()
         if num_samples <= 0:
             raise ValueError("num_samples has to be a positive integer")
         self.num_samples = num_samples
-        self.cache = []
-        self.current_samples = 0
+        self.cache_samples = cache_samples
+        if cache_samples:
+            self.cache = []
+            self.current_samples = 0
+        else:
+            self.batch_size = None
 
     @classmethod
     def from_config(cls, config):
-        return cls(config["num_samples"])
+        return cls(config["num_samples"], config.get("cache_samples", False))
 
     def on_phase_start(self, task) -> None:
-        self.cache = []
-        self.current_samples = 0
+        if self.cache_samples:
+            self.cache = []
+            self.current_samples = 0
+
+    def on_start(self, task) -> None:
+        logging.info(f"Use precise BatchNorm hook. Cache samples? {self.cache_samples}")
 
     def on_step(self, task) -> None:
-        if not task.train or self.current_samples >= self.num_samples:
+        if not task.train:
             return
-        input = recursive_copy_to_device(
-            task.last_batch.sample["input"],
-            non_blocking=True,
-            device=torch.device("cpu"),
-        )
-        self.cache.append(input)
-        self.current_samples += get_batchsize_per_replica(input)
+
+        if self.cache_samples:
+            if self.current_samples >= self.num_samples:
+                return
+            sample = recursive_copy_to_device(
+                task.last_batch.sample,
+                non_blocking=True,
+                device=torch.device("cpu"),
+            )
+            self.cache.append(sample)
+            self.current_samples += get_batchsize_per_replica(sample)
+
+        else:
+            if self.batch_size is not None:
+                return
+
+            self.batch_size = get_batchsize_per_replica(task.last_batch.sample["input"])
 
     def on_phase_end(self, task) -> None:
         if not task.train:
             return
-        iterator = _get_iterator(self.cache, task.use_gpu)
-        num_batches = len(self.cache)
+
+        if self.cache_samples:
+
+            iterator = _get_iterator(self.cache, task.use_gpu)
+            num_batches = len(self.cache)
+
+        else:
+            num_batches = int(math.ceil(self.num_samples / self.batch_size))
+
+            task.build_dataloaders_for_current_phase()
+            task.create_data_iterators()
+            if num_batches > len(task.data_iterator):
+                num_batches = len(task.data_iterator)
+                logging.info(
+                    f"Reduce no. of samples to {num_batches * self.batch_size}"
+                )
+
+            iterator = _get_iterator(task.data_iterator, task.use_gpu)
+
         update_bn_stats(task.base_model, iterator, num_batches)
