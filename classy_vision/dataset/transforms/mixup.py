6938ed3cfc825ce64a898128283853b5102fa083
Zhicheng Yan <zyan3@fb.com>
2021-02-01 20:00:22 -0800

    recursive mixup to input data (#695)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/695
    
    In current mixup implementation, we assume `sample["input"]` is a torch tensor.
    However, in many cases, it can be a dict or a list/tuple. Therefore, we need to generalize to recursive mixup.
    
    Reviewed By: mannatsingh
    
    Differential Revision: D25877855
    
    fbshipit-source-id: f2d0d868685ee38e0f3194fa8aea915a0c87f353

--- classy_vision/dataset/transforms/mixup.py
+++ classy_vision/dataset/transforms/mixup.py
@@ -4,11 +4,36 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+from collections import abc
 from typing import Any, Dict, Optional
 
 import torch
 from classy_vision.generic.util import convert_to_one_hot
 from torch.distributions.beta import Beta
+
+
+def _recursive_mixup(sample: Any, permuted_indices: torch.Tensor, coeff: float):
+    if isinstance(sample, (tuple, list)):
+        mixed_sample = []
+        for s in sample:
+            mixed_sample.append(_recursive_mixup(s, permuted_indices, coeff))
+
+        return mixed_sample if isinstance(sample, list) else tuple(mixed_sample)
+    elif isinstance(sample, abc.Mapping):
+        mixed_sample = {}
+        for key, val in sample.items():
+            mixed_sample[key] = _recursive_mixup(val, permuted_indices, coeff)
+
+        return mixed_sample
+    else:
+        assert torch.is_tensor(sample), "sample is expected to be a pytorch tensor"
+        # Assume training data is at least 3D tensor (i.e. 1D data). We only
+        # mixup content data tensor (e.g. video clip, audio spectrogram), and skip
+        # other tensors, such as frame_idx and timestamp in video clip samples.
+        if sample.ndim >= 3:
+            sample = coeff * sample + (1.0 - coeff) * sample[permuted_indices, :]
+
+        return sample
 
 
 class MixupTransform:
@@ -42,7 +67,10 @@ class MixupTransform:
 
         c = Beta(self.alpha, self.alpha).sample().to(device=sample["target"].device)
         permuted_indices = torch.randperm(sample["target"].shape[0])
-        for key in ["input", "target"]:
-            sample[key] = c * sample[key] + (1.0 - c) * sample[key][permuted_indices, :]
+
+        sample["target"] = (
+            c * sample["target"] + (1.0 - c) * sample["target"][permuted_indices, :]
+        )
+        sample["input"] = _recursive_mixup(sample["input"], permuted_indices, c)
 
         return sample
