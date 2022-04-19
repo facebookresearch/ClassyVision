09ab53794afb16301732436a1d66824b9b9ef36c
Kyle Lee <kyleylee@fb.com>
2022-04-18 21:43:04 -0700

    Add stateless output option for classy block (#785)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ClassyVision/pull/785
    
    X-link: https://github.com/fairinternal/ClassyVision/pull/101
    
    This diff adds a stateless output option within `ClassyBlock.py` when if the environmental variable `CLASSY_BLOCK_STATELESS` is set to "1", `self.output` within `ClassyBlock` never hold the value of the last output (and thus is stateless).  This is useful to prevent certain race conditions, while the default condition will still be maintained if the flag is not enabled to maintain original behavior.
    
    Reviewed By: mannatsingh
    
    Differential Revision: D35662345
    
    fbshipit-source-id: af2a4669c96f1c01454602ebd63de695632adc8a

--- classy_vision/models/classy_block.py
+++ classy_vision/models/classy_block.py
@@ -4,6 +4,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import os
+
 import torch
 import torch.nn as nn
 
@@ -19,11 +21,19 @@ class ClassyBlock(nn.Module):
         self.name = name
         self.output = torch.zeros(0)
         self._module = module
+        # `ClassyBlock` isn't thread safe since it saves state. To avoid
+        # doing this, the recommended workflow is to set `Model.wrapper_cls = None`
+        # before instantiation (see the docs for `ClassyModel`). We support this
+        # environment variable for older use cases but using it is not recommended.
+        self._is_output_stateless = os.environ.get("CLASSY_BLOCK_STATELESS") == "1"
 
     def wrapped_module(self):
         return self._module
 
     def forward(self, input):
-        output = self._module(input)
-        self.output = output
-        return output
+        if self._is_output_stateless:
+            return self._module(input)
+        else:
+            output = self._module(input)
+            self.output = output
+            return output
