82052a5be1df801bc867733a93fbc3dfef72e256
Mannat Singh <mannatsingh@fb.com>
2023-03-16 19:03:11 -0700

    Undelete hydra plugins

---
+++ hydra_plugins/classy_vision_path/classy_vision_path.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from hydra.core.config_search_path import ConfigSearchPath
+from hydra.plugins.search_path_plugin import SearchPathPlugin
+
+
+class ClassyVisionPathPlugin(SearchPathPlugin):
+    def manipulate_search_path(self, search_path: ConfigSearchPath) -> None:
+        search_path.append(
+            provider="classy_vision", path="pkg://classy_vision.hydra.conf"
+        )
